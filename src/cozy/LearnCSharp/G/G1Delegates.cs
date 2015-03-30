4d7dd4fa28c531ec5c430d30179a7c162b0379e3
zapline <278998871@qq.com>
2015-03-30 18:10:48 +0800

    + G1 simple delegate

--- src/cozy/LearnCSharp/G/G1Delegates.cs
+++ src/cozy/LearnCSharp/G/G1Delegates.cs
@@ -6,13 +6,38 @@ using System.Threading.Tasks;
 
 namespace cozy.LearnCSharp.G
 {
+    delegate double DoubleOp(double x);
+
     class G1Delegates
     {
+        static void ProcessAndDisplayNumber(DoubleOp action, double value)
+        {
+            double result = action(value);
+            Console.WriteLine("Value is {0}, result of operation is {1}", value, result);
+        }
+
+        static void SimpleDalagate()
+        {
+            DoubleOp[] operations =
+            {
+                Details.G1MathOperations.MultiplyByTwo,
+                Details.G1MathOperations.Square
+            };
+
+            for (int i = 0; i < operations.Length; ++i)
+            {
+                ProcessAndDisplayNumber(operations[i], 2.0);
+                ProcessAndDisplayNumber(operations[i], 7.94);
+                ProcessAndDisplayNumber(operations[i], 1.414);
+            }
+        }
+
         public static void Cozy()
         {
             Console.WriteLine("\n-----------------------------------------------");
             Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName);
             Console.WriteLine("-----------------------------------------------");
+            SimpleDalagate();
         }
     }
 }
