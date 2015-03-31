81a126a76a428d842f898ea13874b83505a8b332
Max <763305825@qq.com>
2015-03-31 10:41:06 +0800

    B5(Partial关键字的应用)

--- src/cozy/LearnCSharp/B/B5PartialClass.cs
+++ src/cozy/LearnCSharp/B/B5PartialClass.cs
@@ -13,6 +13,18 @@ namespace cozy.LearnCSharp.B
             Console.WriteLine("\n-----------------------------------------------");
             Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName);
             Console.WriteLine("-----------------------------------------------");
+
+            
+            new PartialClass().SayHello();
         }
+    }
+
+    /// <summary>
+    /// Partial关键字可以允许一个类分散在多个文件
+    /// </summary>
+    partial class PartialClass
+    {
+         //定义一空类
+        //SayHello()实现在 ~/Details/B5PartialClass.cs
     }
 }
