4b66e360b85ce59e887968c43a70a9f1d8aec063
zapline <278998871@qq.com>
2015-04-01 09:29:22 +0800

    * J1StandardQuery完成部分

---
+++ src/cozy/LearnCSharp/J/Details/DataLib/Championship.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cozy.LearnCSharp.J.Details.DataLib
+{
+    public class Championship
+    {
+        public int Year { get; set; }
+        public string First { get; set; }
+        public string Second { get; set; }
+        public string Third { get; set; }
+    }
+}
--- src/cozy/LearnCSharp/J/J1StandardQuery.cs
+++ src/cozy/LearnCSharp/J/J1StandardQuery.cs
@@ -13,6 +13,28 @@ namespace cozy.LearnCSharp.J
             Console.WriteLine("\n-----------------------------------------------");
             Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName);
             Console.WriteLine("-----------------------------------------------");
+
+            ///> LINQ一览
+            Details.StandardQuery.LINQQuery();
+            Details.StandardQuery.ExtensionMethods();
+            Details.StandardQuery.DeferredQuery();
+
+            // 筛选
+            // 用索引筛选
+            // 类型筛选
+            // 符合的from子句
+            // 排序
+            // 分组
+            // 对嵌套的对象分组
+            // 内连接
+            // 左外连接
+            // 组连接
+            // 集合方法
+            // 合并
+            // 分区
+            // 聚合操作符
+            // 转换操作符
+            // 生成操作符
         }
     }
 }
