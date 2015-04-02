b2457cbf34e46732b216d70863981ca9edbfa677
kingwl <805037171@163.com>
2015-04-02 16:38:03 +0800

    添加泛型类部分

--- src/cozy/LearnCSharp/D/D1GenericClass.cs
+++ src/cozy/LearnCSharp/D/D1GenericClass.cs
@@ -6,6 +6,29 @@ using System.Threading.Tasks;
 
 namespace cozy.LearnCSharp.D
 {
+    // 声明泛型类    
+    class Object10<T>
+    {
+        private T[] array = new T[10];
+
+        public int ObjSize
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+
+        public T GetObj(int n)
+        {
+            return array[n];
+        }
+
+        public void SetObj(int n, T value)
+        {
+            array[n] = value;
+        }
+    }
     class D1GenericClass
     {
         public static void Cozy()
@@ -13,6 +36,19 @@ namespace cozy.LearnCSharp.D
             Console.WriteLine("\n-----------------------------------------------");
             Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName);
             Console.WriteLine("-----------------------------------------------");
+
+            Generic_Class();
+        }
+
+        public static void Generic_Class()
+        {
+            // 实例化泛型类
+            Object10<int> o10 = new Object10<int>();
+
+            Console.WriteLine(o10.ObjSize);
+            Console.WriteLine(o10.GetObj(0));
+            o10.SetObj(0,42);
+            Console.WriteLine(o10.GetObj(0));
         }
     }
 }
