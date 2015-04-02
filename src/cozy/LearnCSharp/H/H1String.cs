2dc9dc1eb7f4cb9239bf111275ff2a18f4bb0419
zapline <278998871@qq.com>
2015-04-02 16:37:10 +0800

    + 完成H 字符串和正则表达式

--- src/cozy/LearnCSharp/H/H1String.cs
+++ src/cozy/LearnCSharp/H/H1String.cs
@@ -13,6 +13,25 @@ namespace cozy.LearnCSharp.H
             Console.WriteLine("\n-----------------------------------------------");
             Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName);
             Console.WriteLine("-----------------------------------------------");
+
+            string ip = " 11aB3.106.106.";
+            ip += "13";
+            ip = ip.Trim();
+            ip = ip.Replace('a', 'b');
+            ip = ip.ToUpper();
+
+            string ip2 = "1";
+            StringBuilder build = new StringBuilder(100);
+            build.Append(ip);
+            build.Append(ip2);
+            build.Replace("B", "");
+
+            string ip3 = build.ToString();
+            string[] arr = ip3.Split('.');
+            foreach (var x in arr)
+            {
+                Console.WriteLine(x.ToString());
+            }
         }
     }
 }
