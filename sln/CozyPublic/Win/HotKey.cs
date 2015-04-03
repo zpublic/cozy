d64930d1918374edb8d0550319d51e741f390a37
zapline <278998871@qq.com>
2015-04-03 09:08:34 +0800

    + 快捷键ok

---
+++ sln/CozyPublic/Win/HotKey.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyPublic.Win
+{
+    public class HotKey
+    {
+        public const int ModifierAlt        = 1;
+        public const int ModifierControl    = 2;
+        public const int ModifierShift      = 4;
+        public const int ModifierWindows    = 8;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool RegisterHotKey(
+              IntPtr hWnd,
+              int id,
+              uint fsModifiers,
+              uint vk
+              );
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool UnregisterHotKey(
+              IntPtr hWnd,
+              int id
+              );
+    }
+}
---
+++ src/CozyDisplayFusion/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using CozyPublic.Win;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CozyDisplayFusion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int HOTKEY_ID_A   = 1;
+        private const int WM_HOTKEY = 0x0312;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handle)
+        {
+            if (msg == WM_HOTKEY)
+            {
+                if (wParam.ToInt32() == HOTKEY_ID_A)
+                {
+                    StringBuilder s = new StringBuilder();
+                    s.AppendFormat("hwnd:{0},msg:{1},wParam:{2},lParam{3}:,handle:{4}", hwnd, msg, wParam, lParam, handle);
+                    MessageBox.Show(s.ToString());
+                }
+            }
+            return IntPtr.Zero;
+        }
+
+        private void OnSourceInitialized(object sender, EventArgs e)
+        {
+            IntPtr handle = new WindowInteropHelper(this).Handle;
+            HotKey.RegisterHotKey(
+                handle,
+                HOTKEY_ID_A,
+                HotKey.ModifierAlt + HotKey.ModifierControl + HotKey.ModifierShift,
+                0x43);
+            HwndSource source = PresentationSource.FromVisual(this) as HwndSource;
+            source.AddHook(WndProc);
+        }
+    }
+}
