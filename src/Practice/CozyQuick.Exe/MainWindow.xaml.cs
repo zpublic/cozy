5d74c2f361a98dcf5ec0a13b1a8feb902cf3e2bd
zapline <278998871@qq.com>
2015-04-07 22:23:23 +0800

    * 用Reactive做了个拖动的效果

--- src/Practice/CozyQuick.Exe/MainWindow.xaml.cs
+++ src/Practice/CozyQuick.Exe/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,6 +28,28 @@ namespace CozyQuickGui
             InitializeComponent();
             impl = new CozyQuick.Engine.EngineImpl();
             impl.Init();
+
+            var mousedown = Observable.FromEventPattern<MouseButtonEventArgs>(CozyObj, "MouseLeftButtonDown")
+                .Select(x => x.EventArgs.GetPosition(CozyObj));
+
+            var mouseup = Observable.FromEventPattern<MouseButtonEventArgs>(this, "MouseLeftButtonUp");
+
+            var mousemove = Observable.FromEventPattern<MouseEventArgs>(this, "MouseMove")
+                .Select(x => x.EventArgs.GetPosition(this));
+
+            var q = from start in mousedown
+                    from end in mousemove.TakeUntil(mouseup)
+                    select new
+                    {
+                        X = end.X - start.X,
+                        Y = end.Y - start.Y
+                    };
+
+            q.Subscribe(value =>
+            {
+                Canvas.SetLeft(CozyObj, value.X);
+                Canvas.SetTop(CozyObj, value.Y);
+            });
         }
     }
 }
