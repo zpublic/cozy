64df518c68b83b0a7b2464da07c94b3d432f23e1
kingwl <805037171@163.com>
2015-04-09 16:40:05 +0800

    完成数组枚举部分

--- src/Learn/Cozy/LearnCSharp/E/E6Enumeraton.cs
+++ src/Learn/Cozy/LearnCSharp/E/E6Enumeraton.cs
@@ -6,6 +6,42 @@ using System.Threading.Tasks;
 
 namespace Cozy.LearnCSharp.E
 {
+    class CozyCollection
+    {
+        private string[] StrVar = 
+        { 
+            "zapline",
+            "AngryPowman",
+            "Star",
+            "Kingwl",
+            "1900s"
+        };
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            for (int i = 0; i < StrVar.Length; ++i)
+            {
+                yield return StrVar[i];
+            }
+        }
+
+        public IEnumerable<string> Reverse()
+        {
+            for (int i = StrVar.Length - 1; i >= 0; --i)
+            {
+                yield return StrVar[i];
+            }
+        }
+
+        public IEnumerable<string> Subset(int first, int last)
+        {
+            for (int i = first; i < last; ++i)
+            {
+                yield return StrVar[i];
+            }
+        }
+    }
+
     class E6Enumeraton
     {
         public static void Cozy()
@@ -13,6 +49,27 @@ namespace Cozy.LearnCSharp.E
             Console.WriteLine("\n-----------------------------------------------");
             Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName);
             Console.WriteLine("-----------------------------------------------");
+
+            yield_Statement();
+        }
+
+        public static void yield_Statement()
+        {
+            CozyCollection collect = new CozyCollection();
+            foreach(var elemt in collect)
+            {
+                Console.WriteLine(elemt);
+            }
+
+            foreach (var elemt in collect.Reverse())
+            {
+                Console.WriteLine(elemt);
+            }
+
+            foreach (var elemt in collect.Subset(2,4))
+            {
+                Console.WriteLine(elemt);
+            }
         }
     }
 }
