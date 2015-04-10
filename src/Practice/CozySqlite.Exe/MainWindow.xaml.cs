4e2cc35cf7bfdc7ba6b3126ccbbe173b65e57106
Max <763305825@qq.com>
2015-04-10 23:15:57 +0800

    用MVVM模式做皮肤更改

--- src/Practice/CozySqlite.Exe/MainWindow.xaml.cs
+++ src/Practice/CozySqlite.Exe/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MahApps.Metro.Controls;
+﻿using MahApps.Metro;
+using MahApps.Metro.Controls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CozySqlite.Model;
 
 namespace CozySqlite.Exe
 {
@@ -24,6 +26,28 @@ namespace CozySqlite.Exe
         public MainWindow()
         {
             InitializeComponent();
+            ChangedTheme();
+            this.ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+        }
+
+        void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case "SelectedAccent":
+                case "IsLight":
+                    ChangedTheme();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void ChangedTheme()
+        {
+            ThemeManager.ChangeAppStyle(App.Current,
+                                   ThemeManager.GetAccent(ViewModel.SelectedAccent),
+                                   ThemeManager.GetAppTheme(ViewModel.IsLight ? ViewModel.Themes[0] : ViewModel.Themes[1]));
         }
     }
 }
