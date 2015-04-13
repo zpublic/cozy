63aeaf4d161e50b1662c1daba5b177f3275f3dd2
zapline <278998871@qq.com>
2015-04-13 14:50:23 +0800

    * SqlInput增加代码折叠

--- src/Practice/CozySql.Exe/UserControls/SqlInput.xaml.cs
+++ src/Practice/CozySql.Exe/UserControls/SqlInput.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Folding;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace CozySql.Exe.UserControls
 {
@@ -20,9 +23,34 @@ namespace CozySql.Exe.UserControls
     /// </summary>
     public partial class SqlInput : UserControl
     {
+        private FoldingManager foldingManager = null;
+        private AbstractFoldingStrategy foldingStrategy = null;
+
         public SqlInput()
         {
             InitializeComponent();
+            textEditor.Options.ShowTabs = true;
+            textEditor.Options.ShowSpaces = true;
+            textEditor.Options.ConvertTabsToSpaces = true;
+            textEditor.Options.HighlightCurrentLine = true;
+            textEditor.ShowLineNumbers = true;
+
+            foldingManager = FoldingManager.Install(textEditor.TextArea);
+            foldingStrategy = new BraceFoldingStrategy();
+            foldingStrategy.UpdateFoldings(foldingManager, textEditor.Document);
+
+            DispatcherTimer foldingUpdateTimer = new DispatcherTimer();
+            foldingUpdateTimer.Interval = TimeSpan.FromSeconds(2);
+            foldingUpdateTimer.Tick += foldingUpdateTimer_Tick;
+            foldingUpdateTimer.Start();
+        }
+
+        void foldingUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            if (foldingStrategy != null)
+            {
+                foldingStrategy.UpdateFoldings(foldingManager, textEditor.Document);
+            }
         }
     }
 }
