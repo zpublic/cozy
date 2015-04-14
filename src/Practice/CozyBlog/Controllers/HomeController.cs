f0f461bc4ae4b3cdfb98e81486c2fb4b79c55eb7
zapline <278998871@qq.com>
2015-04-14 20:10:33 +0800

    * 修改一些cozyblog的内容

--- src/Practice/CozyBlog/Controllers/HomeController.cs
+++ src/Practice/CozyBlog/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace CozyBlog.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            //ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            //ViewBag.Message = "Your contact page.";
 
             return View();
         }
