4539bcaea2d3ff547c3fc5961783baafbcb6f921
Max <763305825@qq.com>
2015-04-14 10:49:32 +0800

    添加ViewModels目录，修改命名空间

--- src/Practice/CozySql.Model/ViewModels/MainWindowsViewModel.cs
+++ src/Practice/CozySql.Model/ViewModels/MainWindowsViewModel.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace CozySql.Model
+namespace CozySql.Model.ViewModels
 {
     public class MainWindowsViewModel : BaseViewModel
     {
--- src/Practice/CozySql.Model/ViewModels/SqlViewViewModel.cs
+++ src/Practice/CozySql.Model/ViewModels/SqlViewViewModel.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CozySql.Model
+namespace CozySql.Model.ViewModels
 {
     public class SqlViewViewModel : BaseViewModel
     {
