4676c210667b1bbfe91558f98255425dc822f7e3
zapline <278998871@qq.com>
2015-04-14 10:34:47 +0800

    + 完成从mysql数据库取到数据

--- src/Practice/CozySql.Model/SqlViewViewModel.cs
+++ src/Practice/CozySql.Model/SqlViewViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Simple.Data;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
@@ -19,19 +20,27 @@ namespace CozySql.Model
             }
         }
 
+        private static readonly string _ConnectionString =
+            "server=10.20.221.119;user=defend;password=defend;database=defendcloud;";
+
+        private static readonly string _ProviderName = "MySql.Data.MySqlClient";
+
         public void TestData()
         {
+            var db = Database.Opener.OpenConnection(_ConnectionString, _ProviderName);
+            IEnumerable<dynamic> list = db.t20150201file.All()
+                .Where(db.t20150201file.id < 100)
+                .ToList();
+            var firstRow = list.FirstOrDefault() as IDictionary<string, object>;
             var table = new DataTable();
-
-            table.Columns.Add("aaa");
-            table.Columns.Add("bb");
-            table.Columns.Add("cccc");
-
-            table.Rows.Add(new string[] { "data1", "data2", "data3" });
-            table.Rows.Add(new string[] { "data1", "data2", "data3" });
-            table.Rows.Add(new string[] { "data1", "data2", "data3" });
-            table.Rows.Add(new string[] { "data1", "data2", "data3" });
-
+            foreach (var kvp in firstRow)
+            {
+                table.Columns.Add(kvp.Key);
+            }
+            foreach (var row in list.Cast<IDictionary<string, object>>())
+            {
+                table.Rows.Add(row.Values.ToArray());
+            }
             Data = table.DefaultView;
         }
     }
