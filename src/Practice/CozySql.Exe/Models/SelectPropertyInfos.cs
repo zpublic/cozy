cde3f838168d1c7e2770f9365e7e62e735cd101d
Max <763305825@qq.com>
2015-04-16 12:33:25 +0800

    添加缺失的文件

---
+++ src/Practice/CozySql.Exe/Models/SelectPropertyInfos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozySql.Exe.Models
+{
+    public class SelectPropertyInfo
+    {
+        public string Name { get; set; }
+
+        public List<SelectPropertyInfo> Children { get; set; }
+
+        public SelectPropertyInfo()
+        {
+            Children = new List<SelectPropertyInfo>();
+        }
+    }
+}
