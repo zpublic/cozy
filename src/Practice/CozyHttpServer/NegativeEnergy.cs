229432611fe2dac5572d102125781e52e6fe6e88
zapline <278998871@qq.com>
2015-04-23 00:34:10 +0800

    + add CozyHttpServer(含 每天来点负能量)

---
+++ src/Practice/CozyHttpServer/NegativeEnergy.cs
@@ -0,0 +1,30 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyHttpServer
+{
+    public class NegativeEnergy : NancyModule
+    {
+        public static string[] strline = null;
+        public static void LoadData()
+        {
+            string fileName = System.Environment.CurrentDirectory + "/NegativeEnergy.txt";
+            string text = File.ReadAllText(fileName, Encoding.ASCII);
+            strline = File.ReadAllLines(fileName);
+        }
+
+        private Random r = new Random();
+        public NegativeEnergy()
+        {
+            Get["/fnl"] = x =>
+            {
+                return strline.ElementAt(r.Next(strline.Count() - 1));
+            };
+        }
+    }
+}
---
+++ src/Practice/CozyHttpServer/Program.cs
@@ -0,0 +1,60 @@
+﻿using Nancy;
+using Nancy.Hosting.Self;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace CozyHttpServer
+{
+    public class TestModule : NancyModule
+    {
+        public TestModule()
+        {
+            Get["/"] = x =>
+            {
+                return "hello world!";
+            };
+            Get["/test/{name}"] = x =>
+            {
+                return string.Concat("Hello ", x.name);
+            };
+
+            // 每日一撸
+            Get["/lu"] = x =>
+            {
+                var rsp = new
+                {
+                    title = "abc",
+                    url   = "http:://www.laorouji.com",
+                    text  = "aaaaa",
+                };
+                return JsonConvert.SerializeObject(rsp);
+            };
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NegativeEnergy.LoadData();
+
+            using (var host = new NancyHost(new Uri("http://localhost:48360")))
+            {
+                host.Start();
+                try
+                {
+                    Process.Start("http://localhost:48360/fnl");
+                }
+                catch (Exception)
+                {
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
