0a5a828ab18c1fccda3bb09c3828144bdcf9da15
kingwl <805037171@163.com>
2015-04-24 21:10:02 +0800

    添加界面的一部分

--- src/Practice/CozyWifi/ViewModel/MainWindowViewModel.cs
+++ src/Practice/CozyWifi/ViewModel/MainWindowViewModel.cs
@@ -4,11 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using System.Windows.Input;
+using CozyWifi.Command;
+using System.Windows;
 
 namespace CozyWifi.ViewModel
 {
     public class MainWindowViewModel : BaseViewModel
     {
+        #region Property
+        private bool isWifiOpened;
+        public bool IsWifiOpened
+        {
+            get
+            {
+                return isWifiOpened;
+            }
+            set
+            {
+                Set(ref isWifiOpened, value, "WifiOpened");
+            }
+        }
+
+        private string userName;
+        public string UserName
+        {
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                Set(ref userName, value, "UserName");
+            }
+        }
+
+        private string passWord;
+        public string PassWord
+        {
+            get
+            {
+                return passWord;
+            }
+            set
+            {
+                Set(ref passWord, value, "PassWord");
+            }
+        }
+        #endregion
+
+        #region Command
+        private ICommand switchStateCommand;
+        public ICommand SwitchStateCommand
+        {
+            get
+            {
+                return switchStateCommand = switchStateCommand ?? new DelegateCommand(x => IsWifiOpened = !IsWifiOpened);
+            }
+        }
+
+        #endregion
+
         public MainWindowViewModel()
         {
             this.PropertyChanged += PropertyChangedEvent;
@@ -17,7 +73,7 @@ namespace CozyWifi.ViewModel
 
         private void PropertyChangedEvent(object sender, PropertyChangedEventArgs e)
         {
-            if (e.PropertyName == "SelectTreeInfo")
+            if (e.PropertyName == "WifiOpened")
             {
 
             }
