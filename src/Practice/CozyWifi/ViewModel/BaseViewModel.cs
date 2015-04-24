ebe688d4e26e53787a44e2c84d0d387c05ab7771
kingwl <805037171@163.com>
2015-04-24 20:43:57 +0800

    开坑CozyWifi

---
+++ src/Practice/CozyWifi/ViewModel/BaseViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyWifi.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        protected bool Set<T>(ref T field, T value, string propertyName)
+        {
+            if (Equals(field, value)) {
+                return false;
+            }
+
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ src/Practice/CozyWifi/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace CozyWifi.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public MainWindowViewModel()
+        {
+            this.PropertyChanged += PropertyChangedEvent;
+            this.TestData();
+        }
+
+        private void PropertyChangedEvent(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "SelectTreeInfo")
+            {
+
+            }
+        }
+
+        private void TestData()
+        {
+
+        }
+    }
+}
