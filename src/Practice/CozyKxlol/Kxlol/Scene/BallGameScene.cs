16a08ff1ee9c53b9839322488f13eb00e604dd33
kingwl <805037171@163.com>
2015-05-11 12:32:12 +0800

    添加绘制小球部分

---
+++ src/Practice/CozyKxlol/Kxlol/Scene/BallGameScene.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CozyKxlol.Engine;
+
+namespace CozyKxlol.Kxlol.Scene
+{
+    class BallGameScene : CozyScene
+    {
+        public BallGameScene()
+        {
+            Layer = new BallGameSceneLayer();
+        }
+    }
+}
---
+++ src/Practice/CozyKxlol/Kxlol/Scene/BallGameSceneLayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CozyKxlol.Engine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CozyKxlol.Kxlol.Scene
+{
+    class BallGameSceneLayer : CozyLayer
+    {
+        public override void Update(GameTime gameTime)
+        {
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawCircle(new Vector2(300.0f, 300.0f), 20.0f, 20, Color.Red, 20.0f);
+        }
+    }
+}
