97f8b2519b22e2b7421d0761b1c25255846006a7
zapline <278998871@qq.com>
2015-05-11 11:59:19 +0800

    * 拆分CozyKxlol工程

--- src/Practice/CozyKxlol.Engine/Primitives2D.cs
+++ src/Practice/CozyKxlol.Engine/Primitives2D.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace CozyKxlol
+namespace CozyKxlol.Engine
 {
     /// <summary>
     /// </summary>
