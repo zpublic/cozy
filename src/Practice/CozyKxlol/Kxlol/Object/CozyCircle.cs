8a6a2c0d0596372fb7546d8e973e7e38182ec5ba
kingwl <805037171@163.com>
2015-05-11 12:41:29 +0800

    将Circle部分提取出来

---
+++ src/Practice/CozyKxlol/Kxlol/Object/CozyCircle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CozyKxlol.Engine;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace CozyKxlol.Kxlol.Object
+{
+    class CozyCircle : CozyNode
+    {
+        #region Property
+
+        public Vector2 Position { get; set; }
+        public float Radius { get; set; }
+        public Color ColorProperty { get; set; }
+
+        #endregion
+
+        public CozyCircle()
+        {
+            Position        = new Vector2(0.0f, 0.0f);
+            Radius          = 0.0f;
+            ColorProperty   = Color.Black;
+        }
+
+        public CozyCircle(Vector2 pos, float radius, Color color)
+        {
+            Position        = pos;
+            Radius          = radius;
+            ColorProperty   = color;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.DrawCircle(Position, Radius, 20, ColorProperty, 20.0f);
+        }
+    }
+}
--- src/Practice/CozyKxlol/Kxlol/Scene/BallGameSceneLayer.cs
+++ src/Practice/CozyKxlol/Kxlol/Scene/BallGameSceneLayer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CozyKxlol.Kxlol.Object;
 using CozyKxlol.Engine;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -10,13 +11,26 @@ namespace CozyKxlol.Kxlol.Scene
 {
     class BallGameSceneLayer : CozyLayer
     {
+        List<CozyCircle> CircleList = new List<CozyCircle>();
+
+        public BallGameSceneLayer()
+        {
+            var circle1 = new CozyCircle(new Vector2(300.0f, 300.0f), 20.0f, Color.Red);
+            var circle2 = new CozyCircle(new Vector2(400.0f, 300.0f), 20.0f, Color.Blue);
+            CircleList.Add(circle1);
+            CircleList.Add(circle2);
+        }
+
         public override void Update(GameTime gameTime)
         {
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            spriteBatch.DrawCircle(new Vector2(300.0f, 300.0f), 20.0f, 20, Color.Red, 20.0f);
+            foreach (var obj in CircleList)
+            {
+                obj.Draw(gameTime, spriteBatch);
+            }
         }
     }
 }
