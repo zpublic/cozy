884801beff4ddcb9f93ec81234a6a34190cf77bd
kingwl <805037171@163.com>
2015-06-01 23:41:10 +0800

    添加部分帧动画

---
+++ src/Practice/CozyKxlol.Engine/CozyAnimationFrame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CozyKxlol.Engine
+{
+    public class CozyAnimationFrame : ICloneable
+    {
+        public CozyTexture Texture { get; set; }
+        public float DelayUnits { get; set; }
+
+        public static CozyAnimationFrame Create(CozyTexture texture, float delayUnits)
+        {
+            var animate = new CozyAnimationFrame();
+            animate.InitWithTexture(texture, delayUnits);
+            return animate;
+        }
+
+        public static CozyAnimationFrame Create(string path, float delayUnits)
+        {
+            var texture = CozyDirector.Instance.TextureCacheInstance.AddImage(path);
+            var animate = new CozyAnimationFrame();
+            animate.InitWithTexture(texture, delayUnits);
+            return animate;
+        }
+
+        public virtual bool InitWithTexture(CozyTexture texture, float delayUnits)
+        {
+            Texture = texture;
+            DelayUnits = delayUnits;
+            return true;
+        }
+
+        public virtual object Clone()
+        {
+            var texture = Texture.Clone() as CozyTexture;
+            return Create(Texture , DelayUnits);
+        }
+    }
+}
--- src/Practice/CozyKxlol.Engine/CozyTexture.cs
+++ src/Practice/CozyKxlol.Engine/CozyTexture.cs
@@ -2,13 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace CozyKxlol.Engine
 {
-    public class CozyTexture : IDisposable
+    public class CozyTexture : IDisposable, ICloneable
     {
         private Texture2D _Texutre;
+
+        public Vector2 ContentSize
+        {
+            get
+            {
+                return new Vector2(_Texutre.Width, _Texutre.Height);
+            }
+        }
+
+        public CozyTexture()
+        {
+
+        }
+
+        public CozyTexture(Texture2D texture)
+        {
+            _Texutre = texture;
+        }
 
         public CozyTexture(string path)
         {
@@ -23,6 +42,12 @@ namespace CozyKxlol.Engine
         public void Dispose()
         {
             _Texutre.Dispose();
+        }
+
+        public object Clone()
+        {
+            var text = new CozyTexture(this._Texutre);
+            return text; 
         }
     }
 }
