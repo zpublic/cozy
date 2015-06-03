93dbdd06c76cb8cacef0a78a5d6c7eda316f23c9
zapline <278998871@qq.com>
2015-06-03 14:45:09 +0800

    * MapEditor 初期简单设计

--- src/Practice/CozyKxlol.MapEditor/MapEditorScene.cs
+++ src/Practice/CozyKxlol.MapEditor/MapEditorScene.cs
@@ -10,6 +10,9 @@ namespace CozyKxlol.MapEditor
     {
         public MapEditorScene()
         {
+            // 地图编辑器应该分两层
+            // 下层为Engine里的tile绘制层
+            // 上层为编辑功能层，支持鼠标和键盘操作
             this.AddChind(new MapEditorSceneLayer());
         }
     }
---
+++ src/Practice/CozyKxlol.MapEditor/MapEditorSceneTiledLayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CozyKxlol.MapEditor
+{
+    class MapEditorSceneTiledLayer
+    {
+        // update and show current map from TiledMapDataContainer`s data
+    }
+}
