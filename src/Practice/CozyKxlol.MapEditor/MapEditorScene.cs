a459b86cd3afc7d4f4586b92eb1fa14d27147899
zapline <278998871@qq.com>
2015-06-03 14:31:45 +0800

    + KxlolMapEditor

---
+++ src/Practice/CozyKxlol.MapEditor/MapEditorScene.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CozyKxlol.Engine;
+
+namespace CozyKxlol.MapEditor
+{
+    class MapEditorScene : CozyScene
+    {
+        public MapEditorScene()
+        {
+            this.AddChind(new MapEditorSceneLayer());
+        }
+    }
+}
