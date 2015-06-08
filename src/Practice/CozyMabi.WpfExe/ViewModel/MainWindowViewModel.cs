d29bdf27b981b33d9766a1ff0cbc2859ffd9bc28
kingwl <805037171@163.com>
2015-06-08 13:58:08 +0800

    添加简单的冒泡界面

--- src/Practice/CozyMabi.WpfExe/ViewModel/MainWindowViewModel.cs
+++ src/Practice/CozyMabi.WpfExe/ViewModel/MainWindowViewModel.cs
@@ -7,14 +7,45 @@ using System.ComponentModel;
 using System.Windows;
 using System.Windows.Input;
 using CozyMabi.WpfExe.Command;
+using System.Collections.ObjectModel;
+using CozyMabi.WpfExe.Model;
+using CozyMabi.WpfExe.UserControls;
 
 namespace CozyMabi.WpfExe.ViewModel
 {
     public class MainWindowViewModel : BaseViewModel
     {
+
+        #region Property
+
+        private ObservableCollection<UIControlInfo> _MainTabItems;
+        public ObservableCollection<UIControlInfo> MainTabItems
+        {
+            get
+            {
+                return _MainTabItems;
+            }
+            set
+            {
+                Set(ref _MainTabItems, value, "MainTabItems");
+            }
+        }
+
+        #endregion
+
         public MainWindowViewModel()
         {
             this.PropertyChanged += PropertyChangedEvent;
+
+            InitTabItems();
+        }
+
+        void InitTabItems()
+        {
+            MainTabItems = new ObservableCollection<UIControlInfo>(new[]
+            {
+                new UIControlInfo{ Title = "Bubble", Content= new BubbleView() },
+            });
         }
 
         private void PropertyChangedEvent(object sender, PropertyChangedEventArgs e)
---
+++ src/Practice/CozyMabi.WpfExe/ViewModel/UserControlsViewModel/BubbleViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CozyMabi.WpfExe.Command;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Windows;
+
+namespace CozyMabi.WpfExe.ViewModel.UserControlsViewModel
+{
+    public class BubbleViewModel : BaseViewModel
+    {
+        #region Property
+
+        private string _Bubble;
+        public string Bubble
+        {
+            get
+            {
+                return _Bubble;
+            }
+            set
+            {
+                Set(ref _Bubble, value, "Bubble");
+            }
+        }
+
+        #endregion
+
+        #region Command
+
+        private ICommand _SubmitBubble;
+        public ICommand SubmitBubble
+        {
+            get
+            {
+                return _SubmitBubble = _SubmitBubble ?? new DelegateCommand(x =>
+                {
+                    MessageBox.Show(Bubble);
+                });
+            }
+        }
+
+        #endregion
+
+        public BubbleViewModel()
+        {
+            this.PropertyChanged += PropertyChangedEvent;
+        }
+
+        private void PropertyChangedEvent(object sender, PropertyChangedEventArgs e)
+        {
+
+        }
+    }
+}
