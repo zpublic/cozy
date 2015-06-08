8558748d3ceca86d38eae141a87df55ae33517c2
kingwl <805037171@163.com>
2015-06-08 12:48:57 +0800

    添加登陆窗口跳转

--- src/Practice/CozyMabi.WpfExe/ViewModel/MainWindowViewModel.cs
+++ src/Practice/CozyMabi.WpfExe/ViewModel/MainWindowViewModel.cs
@@ -12,66 +12,6 @@ namespace CozyMabi.WpfExe.ViewModel
 {
     public class MainWindowViewModel : BaseViewModel
     {
-
-        #region Property
-
-        private string _AccountEmail = String.Empty;
-        public string AccountEmail
-        {
-            get
-            {
-                return _AccountEmail;
-            }
-            set
-            {
-                Set(ref _AccountEmail, value.Trim(), "AccountEmail");
-            }
-        }
-
-        private string _AccountPassword = String.Empty;
-        public string AccountPassword
-        {
-            get
-            {
-                return _AccountPassword;
-            }
-            set
-            {
-                Set(ref _AccountPassword, value.Trim(), "AccountPassword");
-            }
-        }
-
-        private bool _RememberAccount = false;
-        public bool RememberAccount
-        {
-            get
-            {
-                return _RememberAccount;
-            }
-            set
-            {
-                Set(ref _RememberAccount, value, "RememberAccount");
-            }
-        }
-
-        #endregion
-
-        #region Command
-
-        private ICommand _LoginCommand;
-        public ICommand LoginCommand
-        {
-            get
-            {
-                return _LoginCommand = _LoginCommand ?? new DelegateCommand(x =>
-                {
-                    MessageBox.Show(String.Format("Email : {0} Password : {1} Remember : {2}", AccountEmail, AccountPassword, RememberAccount));
-                });
-            }
-        }
-
-        #endregion
-
         public MainWindowViewModel()
         {
             this.PropertyChanged += PropertyChangedEvent;
@@ -79,7 +19,7 @@ namespace CozyMabi.WpfExe.ViewModel
 
         private void PropertyChangedEvent(object sender, PropertyChangedEventArgs e)
         {
-
+            
         }
     }
 }
