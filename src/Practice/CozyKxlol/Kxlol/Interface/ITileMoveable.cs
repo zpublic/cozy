380544a927bebdb105f5666762721be52e869c84
kingwl <805037171@163.com>
2015-06-24 16:45:09 +0800

    添加tileMove接口

---
+++ src/Practice/CozyKxlol/Kxlol/Interface/ITileMoveable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CozyKxlol.Kxlol.Interface
+{
+    public enum MoveDirection : int 
+    {
+        Left,
+        Right,
+        Up,
+        Down,
+        LeftUp,
+        LeftDown,
+        RightUp,
+        RightDown,
+    }
+    public interface ITileMoveable
+    {
+        void Move(MoveDirection dire);
+    }
+}
