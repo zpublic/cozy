fc90f560d2bd8f551e9c8a79f7ebe2fb0cc24a29
zapline <278998871@qq.com>
2015-06-25 14:46:27 +0800

    + implement CozyLua.Core and tester

---
+++ src/Practice/CozyLua.Core/CozyLuaCore.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyLua.Core
+{
+    public class CozyLuaCore
+    {
+        private NLua.Lua mLua;
+
+        public CozyLuaCore()
+        {
+            mLua = new NLua.Lua();
+        }
+
+        public object[] DoString(string sLua)
+        {
+            return mLua.DoString(sLua);
+        }
+
+        public object[] DoFile(string sLuaFile)
+        {
+            return mLua.DoFile(sLuaFile);
+        }
+
+        public object this [string fullPath]
+        {
+            get
+            {
+                return mLua[fullPath];
+            }
+            set
+            {
+                mLua[fullPath] = value;
+            }
+        }
+
+        public double GetNumber(string fullPath)
+        {
+            return mLua.GetNumber(fullPath);
+        }
+
+        public string GetString(string fullPath)
+        {
+            return mLua.GetString(fullPath);
+        }
+
+        public CozyLuaTable GetTable(string fullPath)
+        {
+            NLua.LuaTable table = mLua.GetTable(fullPath);
+            if (table != null)
+            {
+                return new CozyLuaTable(table);
+            }
+            return null;
+        }
+
+        public CozyLuaFunction GetFunction(string fullPath)
+        {
+            NLua.LuaFunction func = mLua.GetFunction(fullPath);
+            if (func != null)
+            {
+                return new CozyLuaFunction(func);
+            }
+            return null;
+        }
+    }
+}
