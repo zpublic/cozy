03e7ddaa0a5ba384a5aeef85aeb4db844b0419dd
zapline <278998871@qq.com>
2015-06-30 15:14:45 +0800

    * Capture工程部分

---
+++ src/Public/CaptureCpp/CaptureCpp.cpp
@@ -0,0 +1,41 @@
+// CaptureCpp.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "CaptureCpp.h"
+
+
+CAPTURECPP_API int GetDesktopNum(void)
+{
+    int dspNum = ::GetSystemMetrics(SM_CMONITORS);
+    return dspNum;
+}
+
+int gMonitorIndex = 0;
+int gMonitorCount = 0;
+RECT gMonitorSize[10];
+
+BOOL CALLBACK MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData)
+{
+    if (gMonitorIndex < 10)
+    {
+        gMonitorSize[gMonitorIndex] = *lprcMonitor;
+        gMonitorIndex++;
+        gMonitorCount++;
+    }
+    return TRUE;
+}
+
+CAPTURECPP_API bool GetDesktopSize(int nIndex, int* w, int* h)
+{
+    gMonitorIndex = 0;
+    gMonitorCount = 0;
+    ::EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, 0);
+    if (nIndex < gMonitorCount)
+    {
+        *w = gMonitorSize[nIndex].right - gMonitorSize[nIndex].left;
+        *h = gMonitorSize[nIndex].bottom - gMonitorSize[nIndex].top;
+        return true;
+    }
+    return false;
+}
---
+++ src/Public/CaptureCpp/CaptureCpp.h
@@ -0,0 +1,15 @@
+// The following ifdef block is the standard way of creating macros which make exporting 
+// from a DLL simpler. All files within this DLL are compiled with the CAPTURECPP_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see 
+// CAPTURECPP_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef CAPTURECPP_EXPORTS
+#define CAPTURECPP_API __declspec(dllexport)
+#else
+#define CAPTURECPP_API __declspec(dllimport)
+#endif
+
+CAPTURECPP_API int GetDesktopNum(void);
+
+CAPTURECPP_API bool GetDesktopSize(int nIndex, int* w, int* h);
