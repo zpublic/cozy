1bef4b93af4ee03c88533f4f932cf4bc12f9a368
zapline <278998871@qq.com>
2015-06-30 11:05:26 +0800

    + 开坑CozyWallpaper

---
+++ src/Practice/CozyWallpaper.Core/WallpaperNative.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyWallpaper.Core
+{
+    class WallpaperNative
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+        public const int SPI_SETDESKWALLPAPER = 20;
+        public const int SPIF_SENDCHANGE = 0x2;
+    }
+}
