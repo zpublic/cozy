e624f12eb483cef94d9302ead521a93ee76312d7
kingwl <805037171@163.com>
2015-07-03 23:30:38 +0800

    整理输出目录

--- src/Public/FileTester/Ext/FileUtil.cs
+++ src/Public/FileTester/Ext/FileUtil.cs
@@ -10,10 +10,10 @@ namespace FileTester.Ext
     public class FileUtil
     {
         public delegate void FileEnumFunc(IntPtr ptr, bool IsFolder);
-        [DllImport(@"../FileUtilCpp/FileUtilCpp.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(@"../../FileUtilCpp/Debug/FileUtilCpp.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
         public static extern void FileEnum(string Path, FileEnumFunc func);
 
-        [DllImport(@"../FileUtilCpp/FileUtilCpp.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(@"../../FileUtilCpp/Debug/FileUtilCpp.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
         public static extern bool FileDelete(string Path);
     }
 }
