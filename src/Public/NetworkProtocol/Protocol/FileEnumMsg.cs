1dfa2d5c8dff1f12ab963db8a20a8ec73f8bb63d
kingwl <805037171@163.com>
2015-07-03 23:50:43 +0800

    添加Protocol和消息

---
+++ src/Public/NetworkProtocol/Protocol/FileEnumMsg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkHelper.Messages;
+using Lidgren.Network;
+
+namespace NetworkProtocol.Protocol
+{
+    public struct FileEnumMsg : IMessage
+    {
+        public uint Id { get { return 0; } }
+
+        List<Tuple<string, uint, bool>> FileInfoList { get; set; }
+
+        public void Write(NetOutgoingMessage om)
+        {
+            if(FileInfoList != null)
+            {
+                om.Write(FileInfoList.Count);
+                foreach(var obj in FileInfoList)
+                {
+                    om.Write(obj.Item1);
+                    om.Write(obj.Item2);
+                    om.Write(obj.Item3);
+                }
+            }
+        }
+
+        public void Read(NetIncomingMessage im)
+        {
+            if(FileInfoList == null)
+            {
+                FileInfoList = new List<Tuple<string, uint, bool>>();
+            }
+            int ListCount = im.ReadInt32();
+            for(int i = 0; i < ListCount; ++i)
+            {
+                string Name = im.ReadString();
+                uint Size   = im.ReadUInt32();
+                bool IsDire = im.ReadBoolean();
+                FileInfoList.Add(Tuple.Create<string, uint, bool>(Name, Size, IsDire));
+            }
+        }
+    }
+}
