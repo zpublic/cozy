a7be379d767ebb23e61be521be8635f164ef85da
kingwl <805037171@163.com>
2015-07-05 00:41:25 +0800

    添加ProcessTester

---
+++ src/Public/ProcessTester/Ext/ProcessUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ProcessTester.Ext
+{
+    public static class ProcessUtil
+    {
+        public delegate void ProcessEnumFunc(uint pid);
+
+        [DllImport(@"../../ProcessUtilCpp/Debug/ProcessUtilCpp.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ProcessEnum(ProcessEnumFunc func);
+    }
+}
---
+++ src/Public/ProcessTester/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ProcessTester.Ext;
+using ProcessTester.Model;
+using System.Collections.ObjectModel;
+
+namespace ProcessTester.ViewModel
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private ObservableCollection<ProcessInfo> _ProcessInfoList = new ObservableCollection<ProcessInfo>();
+
+        public ObservableCollection<ProcessInfo> ProcessInfoList
+        {
+            get
+            {
+                return _ProcessInfoList;
+            }
+            set
+            {
+                Set(ref _ProcessInfoList, value, "ProcessInfoList");
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            TestData();
+        }
+
+        private void TestData()
+        {
+            ProcessUtil.ProcessEnum((pid) =>
+            {
+                ProcessInfoList.Add(
+                new ProcessInfo
+                {
+                    Name    = "",
+                    Pid     = pid,
+                }
+                );
+            });
+        }
+    }
+}
