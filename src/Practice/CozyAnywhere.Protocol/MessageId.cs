b381d317df71ffebba16e1900a63318fda50de16
kingwl <805037171@163.com>
2015-07-05 16:42:43 +0800

    添加文件删除和进程结束消息

--- src/Practice/CozyAnywhere.Protocol/MessageId.cs
+++ src/Practice/CozyAnywhere.Protocol/MessageId.cs
@@ -2,9 +2,11 @@
 {
     public static class MessageId
     {
-        public const uint FileEnumMessage       = 1001;
-        public const uint FileEnumMessageRsp    = 1002;
-        public const uint ProcessEnumMessage    = 1003;
-        public const uint ProcessEnumMessageRsp = 1004;
+        public const uint FileEnumMessage           = 1001;
+        public const uint FileEnumMessageRsp        = 1002;
+        public const uint ProcessEnumMessage        = 1003;
+        public const uint ProcessEnumMessageRsp     = 1004;
+        public const uint ProcessTerminateMessage   = 1005;
+        public const uint FileDeleteMessage         = 1006;
     }
 }
