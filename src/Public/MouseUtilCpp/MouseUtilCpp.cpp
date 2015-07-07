509249b9073b9ce685571ec688ad7b687604c705
kingwl <805037171@163.com>
2015-07-07 20:15:17 +0800

    添加MouseUtil

---
+++ src/Public/MouseUtilCpp/MouseUtilCpp.cpp
@@ -0,0 +1,117 @@
+// MouseUtilCpp.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "MouseUtilCpp.h"
+
+MOUSEUTILCPP_API CMouseUtilCpp CMouseUtilCppInstance;
+
+CMouseUtilCpp::CMouseUtilCpp(void)
+{
+
+}
+
+bool CMouseUtilCpp::GetCursorPosition(long* lpXPosition, long* lpYPosition)
+{
+    POINT p;
+    bool bResult = (::GetCursorPos(&p) == TRUE);
+
+    if (!bResult) return false;
+    *lpXPosition = p.x;
+    *lpYPosition = p.y;
+    return true;
+}
+
+bool CMouseUtilCpp::SetCursorPosition(int iXPosition, int iYPosition)
+{
+    return (::SetCursorPos(iXPosition, iYPosition) == TRUE);
+}
+
+bool CMouseUtilCpp::CursorClip(const RECT* rect)
+{
+    return (::ClipCursor(rect) == TRUE);
+}
+
+void CMouseUtilCpp::MouseEvent(
+    DWORD dwFlags, DWORD dx, DWORD dy, 
+    DWORD dwData, ULONG_PTR dwExtraInfo)
+{
+    ::mouse_event(dwFlags, dx, dy, dwData, dwExtraInfo);
+}
+
+void CMouseUtilCpp::MouseClick(BUTTON key, DWORD dx, DWORD dy)
+{
+    DWORD dwKeyDownTag;
+    DWORD dwKeyUpTag;
+
+    switch (key)
+    {
+    case LEFT:
+        dwKeyDownTag    = MOUSEEVENTF_LEFTDOWN;
+        dwKeyUpTag      = MOUSEEVENTF_LEFTUP;
+        break;
+    case RIGHT:
+        dwKeyDownTag    = MOUSEEVENTF_RIGHTDOWN;
+        dwKeyUpTag      = MOUSEEVENTF_RIGHTUP;
+        break;
+    case MIDDLE:
+        dwKeyDownTag    = MOUSEEVENTF_XDOWN;
+        dwKeyUpTag      = MOUSEEVENTF_XUP;
+        break;
+    default:
+        break;
+    }
+
+    MouseEvent(dwKeyDownTag, dx, dy, 0, 0);
+    MouseEvent(dwKeyUpTag, dx, dy, 0, 0);
+}
+
+MOUSEUTILCPP_API bool GetCursorPosition(long* lpXPosition, long* lpYPosition)
+{
+    return CMouseUtilCppInstance.GetCursorPosition(lpXPosition, lpYPosition);
+}
+
+MOUSEUTILCPP_API bool CursorClip(long lLeft, long lTop, long lRight, long lBottom)
+{
+    RECT rect;
+    rect.left   = lLeft;
+    rect.right  = lRight;
+    rect.top    = lTop;
+    rect.bottom = lBottom;
+    return CMouseUtilCppInstance.CursorClip(&rect);
+}
+
+MOUSEUTILCPP_API bool CursorUnClip()
+{
+    return CMouseUtilCppInstance.CursorClip(nullptr);
+}
+
+MOUSEUTILCPP_API void MouseEventMouseEvent(DWORD dwFlags, DWORD dx, DWORD dy, DWORD dwData, ULONG_PTR dwExtraInfo)
+{
+    CMouseUtilCppInstance.MouseEvent(dwFlags, dx, dy, dwData, dwExtraInfo);
+}
+
+MOUSEUTILCPP_API bool SetCursorPosition(int iXPosition, int iYPosition)
+{
+    return CMouseUtilCppInstance.SetCursorPosition(iXPosition, iYPosition);
+}
+
+MOUSEUTILCPP_API void MouseClick(BUTTON key, DWORD dx, DWORD dy)
+{
+    CMouseUtilCppInstance.MouseClick(key, dx, dy);
+}
+
+MOUSEUTILCPP_API void LeftClick(DWORD dx, DWORD dy)
+{
+    CMouseUtilCppInstance.MouseClick(BUTTON::LEFT, dx, dy);
+}
+
+MOUSEUTILCPP_API void RightClick(DWORD dx, DWORD dy)
+{
+    CMouseUtilCppInstance.MouseClick(BUTTON::RIGHT, dx, dy);
+}
+
+MOUSEUTILCPP_API void MiddleClick(DWORD dx, DWORD dy)
+{
+    CMouseUtilCppInstance.MouseClick(BUTTON::MIDDLE, dx, dy);
+}
---
+++ src/Public/MouseUtilCpp/MouseUtilCpp.h
@@ -0,0 +1,56 @@
+// The following ifdef block is the standard way of creating macros which make exporting
+// from a DLL simpler. All files within this DLL are compiled with the MOUSEUTILCPP_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see
+// MOUSEUTILCPP_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef MOUSEUTILCPP_EXPORTS
+#define MOUSEUTILCPP_API __declspec(dllexport)
+#else
+#define MOUSEUTILCPP_API __declspec(dllimport)
+#endif
+
+#include "windows.h"
+
+enum BUTTON : DWORD
+{
+    LEFT    = 0,
+    RIGHT   = 1,
+    MIDDLE  = 2,
+};
+
+class CMouseUtilCpp
+{
+public:
+    CMouseUtilCpp(void);
+
+    bool GetCursorPosition(long* lpXPosition, long* lpYPosition);
+
+    bool SetCursorPosition(int iXPosition, int iYPosition);
+
+    bool CursorClip(const RECT* rect);
+
+    void MouseEvent(DWORD dwFlags, DWORD dx, DWORD dy, DWORD dwData, ULONG_PTR dwExtraInfo);
+
+    void MouseClick(BUTTON key, DWORD dx, DWORD dy);
+};
+
+extern MOUSEUTILCPP_API CMouseUtilCpp CMouseUtilCppInstance;
+
+extern "C" MOUSEUTILCPP_API bool GetCursorPosition(long* lpXPosition, long* lpYPosition);
+
+extern "C" MOUSEUTILCPP_API bool SetCursorPosition(int iXPosition, int iYPosition);
+
+extern "C" MOUSEUTILCPP_API bool CursorClip(long lLeft, long lTop, long lRight, long lBottom);
+
+extern "C" MOUSEUTILCPP_API bool CursorUnClip();
+
+extern "C" MOUSEUTILCPP_API void MouseEventMouseEvent(DWORD dwFlags, DWORD dx, DWORD dy, DWORD dwData, ULONG_PTR dwExtraInfo);
+
+extern "C" MOUSEUTILCPP_API void MouseClick(BUTTON key, DWORD dx, DWORD dy);
+
+extern "C" MOUSEUTILCPP_API void LeftClick(DWORD dx, DWORD dy);
+
+extern "C" MOUSEUTILCPP_API void RightClick(DWORD dx, DWORD dy);
+
+extern "C" MOUSEUTILCPP_API void MiddleClick(DWORD dx, DWORD dy);
