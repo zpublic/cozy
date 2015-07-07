1262380c70428b2caf977c76cd1ae5be6791fbb6
kingwl <805037171@163.com>
2015-07-07 20:15:27 +0800

    修改之前的注释错误

--- src/Public/ProcessUtilCpp/ProcessUtilCpp.h
+++ src/Public/ProcessUtilCpp/ProcessUtilCpp.h
@@ -1,5 +1,5 @@
 // The following ifdef block is the standard way of creating macros which make exporting
-// from a DLL simpler. All files within this DLL are compiled with the FILEUTILCPP_EXPORTS
+// from a DLL simpler. All files within this DLL are compiled with the PROCESSUTILCPP_EXPORTS
 // symbol defined on the command line. This symbol should not be defined on any project
 // that uses this DLL. This way any other project whose source files include this file see
 // PROCESSUTILCPP_API functions as being imported from a DLL, whereas this DLL sees symbols
