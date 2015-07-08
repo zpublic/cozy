683acf785ec38659bb0d68680a520f9f5c16e350
kingwl <805037171@163.com>
2015-07-08 20:26:01 +0800

    添加ClientCore和ServerCore的一部分

---
+++ src/Practice/CozyAnywhere.ClientCore/ClientMessage.cs
@@ -0,0 +1,22 @@
+﻿using CozyAnywhere.Protocol;
+using CozyAnywhere.Protocol.Messages;
+using NetworkHelper;
+using NetworkProtocol;
+
+namespace CozyAnywhere.ClientCore
+{
+    public partial class AnywhereClient
+    {
+        public void InitClientMessage()
+        {
+            MessageReader.RegisterType<FileEnumMessageRsp>(MessageId.FileEnumMessageRsp);
+        }
+
+        public void OnFileEnumMessageRsp(IMessage msg)
+        {
+            var rspMsg = (FileEnumMessageRsp)msg;
+
+            // TODO
+        }
+    }
+}
