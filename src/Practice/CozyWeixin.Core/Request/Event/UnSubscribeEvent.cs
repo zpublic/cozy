96e1049720c181828b0c92a45ee7ecfe7efc5d04
zapline <278998871@qq.com>
2015-07-24 18:29:37 +0800

    * 初始化一下CozyWeixin.Core的结构

---
+++ src/Practice/CozyWeixin.Core/Request/Event/UnSubscribeEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyWeixin.Core.Request.Event
+{
+    class UnSubscribeEvent
+    {
+    }
+}
