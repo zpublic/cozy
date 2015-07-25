4e709ddbf873e2ca142b0ba1b2b154e77402ae90
Max <763305825@qq.com>
2015-07-25 12:57:02 +0800

    添加CozyWeixin.Mvc

---
+++ src/Practice/CozyWexin.Mvc/ApiControllers/WeixinController.cs
@@ -0,0 +1,21 @@
+﻿using CozyWeixin.Core.Account.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CozyWexin.Mvc.ApiControllers {
+
+    [RoutePrefix("api/weixin")]
+    public class WeixinController : ApiController {
+
+        [HttpGet]
+        [Route("test")]
+        public string TestFunc() {
+            return "test";
+        }
+
+    }
+}
