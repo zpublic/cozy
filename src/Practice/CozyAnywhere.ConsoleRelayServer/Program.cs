48c0e1b26d104aea02f588a586b04f3d33913c11
kingwl <805037171@163.com>
2015-07-25 21:46:46 +0800

    添加ConsoleRelayServer

---
+++ src/Practice/CozyAnywhere.ConsoleRelayServer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetworkRelayServer;
+using CozyAnywhere.RelayServerCore;
+
+namespace CozyAnywhere.ConsoleRelayServer
+{
+    public class Program
+    {
+        public static ServerRelay server = new ServerRelay(1000, 48360);
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                server.EnterMainLoop();
+            }
+        }
+    }
+}
