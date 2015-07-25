229168ea75ed62014f1373dce4872f37e5b749ec
Max <763305825@qq.com>
2015-07-25 11:44:39 +0800

    加入webAPi

---
+++ src/Practice/CozyWeixin.WebPage/ApiControllers/WeixinController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CozyWeixin.WebPage.ApiControllers
+{
+    [Route("api/[controller]")]
+    public class WeixinController : Controller
+    {
+        const string appId = "wx84e01be9fa8ff6c7";
+        const string appSecret = "9f1291356eb2da494b2123f7747cd4a0";
+
+        [HttpGet]
+        [Route("test1")]
+        public string GetToken()
+        {
+            return "Hello";
+        }
+
+    }
+}
