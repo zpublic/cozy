e1d05279dce920507e7f9cba2abff87a8d065ab5
kingwl <805037171@163.com>
2015-07-28 20:48:49 +0800

    添加PluginHelper

---
+++ src/Public/PluginHelper/ArgsFactoryLoader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace PluginHelper
+{
+    public static class ArgsFactoryLoader
+    {
+        public static List<Tuple<string, Type>> LoadArgsFactory(Assembly asm, string ns)
+        {
+            var factoryList = new List<Tuple<string, Type>>();
+            foreach (var t in asm.GetTypes())
+            {
+                if (t.Namespace == ns)
+                {
+                    var name = t.Name.Substring(0, t.Name.Length - 11);
+                    factoryList.Add(Tuple.Create(name, t));
+                }
+            }
+            return factoryList;
+        }
+    }
+}
---
+++ src/Public/PluginHelper/PluginCommandMaker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace PluginHelper
+{
+    public class PluginCommandMaker
+    {
+        private Dictionary<string, MethodInfo> MakerDictionary = new Dictionary<string, MethodInfo>();
+
+        public void LoadCommand(Assembly asm, string pluginName)
+        {
+            var pluginType = asm.GetType(pluginName);
+            var methods = pluginType.GetMethods();
+            foreach (var method in methods)
+            {
+                if (method.IsStatic
+                    && method.ReturnType == typeof(string)
+                    && method.Name.StartsWith("Make")
+                    && method.Name.EndsWith("Command"))
+                {
+                    var name = method.Name.Substring(4, method.Name.Length - 11);
+                    MakerDictionary[name] = method;
+                }
+            }
+        }
+    }
+}
