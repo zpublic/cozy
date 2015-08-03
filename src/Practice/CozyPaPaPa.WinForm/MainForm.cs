a7016d4de25c5589045c9fb876b0e26cea832670
Max <763305825@qq.com>
2015-08-03 17:18:27 +0800

    测试失败

---
+++ src/Practice/CozyPaPaPa.WinForm/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using CozyPaPaPa.Core;
+
+namespace CozyPaPaPa.WinForm {
+
+    public partial class MainForm : MetroForm {
+
+        public MainForm() {
+            InitializeComponent();
+            var pa = new Pa();
+            pa.KeyPressEvent += Pa_KeyPressEvent;
+            pa.KeyDownEvent += Pa_KeyDownEvent;
+        }
+
+        private void Pa_KeyDownEvent(object sender, KeyEventArgs e) {
+            MessageBox.Show(e.KeyCode.ToString());
+        }
+
+        private void Pa_KeyPressEvent(object sender, KeyPressEventArgs e) {
+            MessageBox.Show(e.KeyChar.ToString());
+        }
+    }
+}
--- src/Practice/CozyPaPaPa.WinForm/Program.cs
+++ src/Practice/CozyPaPaPa.WinForm/Program.cs
@@ -13,7 +13,7 @@ namespace CozyPaPaPa.WinForm {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
