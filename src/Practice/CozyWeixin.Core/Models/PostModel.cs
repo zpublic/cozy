5153995afb4e4458d136ba59f054a716db0b4bd4
Max <763305825@qq.com>
2015-08-03 22:37:02 +0800

    保存上礼拜在本地的修改

---
+++ src/Practice/CozyWeixin.Core/Models/PostModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyWeixin.Core.Models {
+
+    public class PostModel {
+
+        public string Signature { get; set; }
+        public string Msg_Signature { get; set; }
+        public string Timestamp { get; set; }
+        public string Nonce { get; set; }
+
+        public string Token { get; set; }
+        public string EncodingAESKey { get; set; }
+        public string AppId { get; set; }
+    }
+}
---
+++ src/Practice/CozyWeixin.Core/Models/ReceiveModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyWeixin.Core.Models {
+
+    public class ReceiveModel {
+
+        public string ToUserName { get; set; }
+        public string FromUserName { get; set; }
+        public string CreateTime { get; set; }
+        public string MsgType { get; set; }
+        public string Content { get; set; }
+        public string MsgId { get; set; }
+    }
+}
