589bd3ec20e24df93dbf4f11afc456f955e23b29
zapline <278998871@qq.com>
2015-08-11 17:24:16 +0800

    + create CozyNote.ConsoleClient

---
+++ src/Practice/CozyNote.ConsoleClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyNote.ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+        }
+    }
+}
