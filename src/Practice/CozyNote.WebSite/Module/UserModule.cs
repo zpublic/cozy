271158ceac44c1c8d440e02c29194dfd7f7f2e9a
zapline <278998871@qq.com>
2015-08-11 15:31:35 +0800

    + add CozyNote.WebSite

---
+++ src/Practice/CozyNote.WebSite/Module/UserModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace CozyNote.WebSite.Module
+{
+    public class UserModule : NancyModule
+    {
+        public UserModule() : base("/user")
+        {
+            Get["/{name}/notebook"] = x =>
+            {
+                return string.Concat("Hello ", x.name);
+            };
+        }
+    }
+}
---
+++ src/Practice/CozyNote.WebSite/WebSiteHost.cs
@@ -0,0 +1,30 @@
+﻿using Nancy;
+using Nancy.Hosting.Self;
+using System;
+
+namespace CozyNote.WebSite
+{
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = x =>
+            {
+                return "hello world!";
+            };
+        }
+    }
+
+    class WebSiteHost
+    {
+        public static void Run(string uri)
+        {
+            HostConfiguration hostConfigs = new HostConfiguration()
+            {
+                UrlReservations = new UrlReservations() { CreateAutomatically = true }
+            };
+            var host = new NancyHost(new Uri(uri), new DefaultNancyBootstrapper(), hostConfigs);
+            host.Start();
+        }
+    }
+}
