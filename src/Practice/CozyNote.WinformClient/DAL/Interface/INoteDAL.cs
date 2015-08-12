3852b72c443411a08d5cc50fa2763101a2dcc00f
Max <763305825@qq.com>
2015-08-12 23:36:02 +0800

    写了一点，睡觉

---
+++ src/Practice/CozyNote.WinformClient/DAL/Interface/INoteDAL.cs
@@ -0,0 +1,22 @@
+﻿using CozyNote.WinformClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace CozyNote.WinformClient.DAL {
+
+    public interface INoteDAL {
+
+        List<NoteModel> GetAll();
+
+        NoteModel Get(Guid Id);
+
+        List<NoteModel> Query(Expression<Func<NoteModel, bool>> func);
+
+        Guid Insert(NoteModel model);
+
+        bool Update(NoteModel model);
+
+        bool Delete(Guid Id);
+    }
+}
---
+++ src/Practice/CozyNote.WinformClient/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CozyNote.WinformClient.Models {
+
+    public class UserModel {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
