90023e61d1ad7b3446c1827210d77812ad869cd2
kingwl <805037171@163.com>
2015-08-14 10:21:35 +0800

    添加一个特定网站的解析

--- src/Practice/CozySpider.Core/Reader/DefaultReader.cs
+++ src/Practice/CozySpider.Core/Reader/DefaultReader.cs
@@ -14,13 +14,14 @@ namespace CozySpider.Core.Reader
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url.Trim());
             request.AllowAutoRedirect = true;
-            request.UserAgent = @"Mozilla/5.0 (Windows NT 5.2) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.122 Safari/534.30";
+            request.UserAgent = @"Mozilla/5.2 (Windows NT 6.1) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.122 Safari/534.30";
             request.Accept = @"*/*";
+            request.Timeout = 3000;
 
             WebResponse respone = request.GetResponse();
             using (var rspStream = respone.GetResponseStream())
             {
-                StreamReader reader = new StreamReader(rspStream, Encoding.Default);
+                StreamReader reader = new StreamReader(rspStream, Encoding.UTF8);
                 string result = reader.ReadToEnd();
 
                 return result;
