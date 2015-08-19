8853c9a3719bbaa916e57613654d013f804ca3cb
zapline <278998871@qq.com>
2015-08-19 12:39:45 +0800

    * CozyDungeon 泡面撸撸大作战

--- src/Practice/CozyDungeon.Game.Component.Card.Tester/Program.cs
+++ src/Practice/CozyDungeon.Game.Component.Card.Tester/Program.cs
@@ -15,7 +15,7 @@ namespace CozyDungeon.Game.Component.Card.Tester
             RoleCard card1 = new RoleCard()
             {
                 Id = 1,
-                Name = "c1",
+                Name = "王文撸",
                 Desc = "card1",
                 Element = Enum.FiveLine.Gold,
                 HP = 10,
@@ -25,7 +25,7 @@ namespace CozyDungeon.Game.Component.Card.Tester
             RoleCard card2 = new RoleCard()
             {
                 Id = 2,
-                Name = "c2",
+                Name = "愤怒的泡面",
                 Desc = "card2",
                 Element = Enum.FiveLine.Wood,
                 HP = 10,
@@ -36,21 +36,23 @@ namespace CozyDungeon.Game.Component.Card.Tester
             RoleCard r1 = (RoleCard)card1.Clone();
             RoleCard r2 = (RoleCard)card2.Clone();
 
-            Console.WriteLine("r1.hp={0} r2.hp={1}", r1.HP, r2.HP);
-            RoleCardLogic.Attack(r1, r2);
-            Console.WriteLine(RoleCardLogic.IsAlive(r2));
-            Console.WriteLine("r1.hp={0} r2.hp={1}", r1.HP, r2.HP);
-            RoleCardLogic.Attack(r1, r2);
-            Console.WriteLine("r1.hp={0} r2.hp={1}", r1.HP, r2.HP);
-            RoleCardLogic.Attack(r1, r2);
-            Console.WriteLine("r1.hp={0} r2.hp={1}", r1.HP, r2.HP);
-            RoleCardLogic.Attack(r1, r2);
-            Console.WriteLine("r1.hp={0} r2.hp={1}", r1.HP, r2.HP);
-            RoleCardLogic.Attack(r2, r1);
-            Console.WriteLine("r1.hp={0} r2.hp={1}", r1.HP, r2.HP);
-            RoleCardLogic.Attack(r2, r1);
-            Console.WriteLine(RoleCardLogic.IsAlive(r2));
-            Console.WriteLine("r1.hp={0} r2.hp={1}", r1.HP, r2.HP);
+            bool b = false;
+            while (RoleCardLogic.IsAlive(r1) && RoleCardLogic.IsAlive(r2))
+            {
+                if (b)
+                {
+                    Console.WriteLine("{0} attack {1}", r1.Name, r2.Name);
+                    RoleCardLogic.Attack(r1, r2);
+                    Console.WriteLine("撸撸.hp={0} 泡面.hp={1}\n", r1.HP, r2.HP);
+                }
+                else
+                {
+                    Console.WriteLine("{0} attack {1}", r2.Name, r1.Name);
+                    RoleCardLogic.Attack(r2, r1);
+                    Console.WriteLine("撸撸.hp={0} 泡面.hp={1}\n", r1.HP, r2.HP);
+                }
+                b = !b;
+            }
         }
     }
 }
