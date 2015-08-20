c49454852ead1f56a74e4f7a144800e89b60e88a
kingwl <805037171@163.com>
2015-08-20 18:35:14 +0800

    修改控件名称

--- src/Practice/CozyDungeon.RoleCardEditor/EditorForm.Designer.cs
+++ src/Practice/CozyDungeon.RoleCardEditor/EditorForm.Designer.cs
@@ -28,35 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.DescBox = new System.Windows.Forms.TextBox();
             this.cardPictureBox = new System.Windows.Forms.PictureBox();
-            this.metroButton1 = new MetroFramework.Controls.MetroButton();
-            this.metroButton2 = new MetroFramework.Controls.MetroButton();
-            this.metroButton3 = new MetroFramework.Controls.MetroButton();
-            this.metroTabControl1 = new MetroFramework.Controls.MetroTabControl();
+            this.OpenButton = new MetroFramework.Controls.MetroButton();
+            this.SaveButton = new MetroFramework.Controls.MetroButton();
+            this.CloseButton = new MetroFramework.Controls.MetroButton();
+            this.CardTabControl = new MetroFramework.Controls.MetroTabControl();
             this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
             this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
             this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
             this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
-            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.LevelBox = new MetroFramework.Controls.MetroComboBox();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.IDBox = new System.Windows.Forms.TextBox();
+            this.HPBox = new System.Windows.Forms.TextBox();
             this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.ATKBox = new System.Windows.Forms.TextBox();
             this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.DEFBox = new System.Windows.Forms.TextBox();
             this.metroLabel7 = new MetroFramework.Controls.MetroLabel();
             ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox3
+            // DescBox
             // 
-            this.textBox3.Location = new System.Drawing.Point(343, 287);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(198, 92);
-            this.textBox3.TabIndex = 8;
+            this.DescBox.Location = new System.Drawing.Point(343, 287);
+            this.DescBox.Multiline = true;
+            this.DescBox.Name = "DescBox";
+            this.DescBox.Size = new System.Drawing.Size(198, 92);
+            this.DescBox.TabIndex = 8;
             // 
             // cardPictureBox
             // 
@@ -69,42 +69,42 @@
             this.cardPictureBox.TabIndex = 9;
             this.cardPictureBox.TabStop = false;
             // 
-            // metroButton1
+            // OpenButton
             // 
-            this.metroButton1.Location = new System.Drawing.Point(24, 63);
-            this.metroButton1.Name = "metroButton1";
-            this.metroButton1.Size = new System.Drawing.Size(75, 23);
-            this.metroButton1.TabIndex = 10;
-            this.metroButton1.Text = "打开";
-            this.metroButton1.UseSelectable = true;
+            this.OpenButton.Location = new System.Drawing.Point(24, 63);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenButton.TabIndex = 10;
+            this.OpenButton.Text = "打开";
+            this.OpenButton.UseSelectable = true;
             // 
-            // metroButton2
+            // SaveButton
             // 
-            this.metroButton2.Location = new System.Drawing.Point(105, 62);
-            this.metroButton2.Name = "metroButton2";
-            this.metroButton2.Size = new System.Drawing.Size(75, 23);
-            this.metroButton2.TabIndex = 10;
-            this.metroButton2.Text = "保存";
-            this.metroButton2.UseSelectable = true;
+            this.SaveButton.Location = new System.Drawing.Point(105, 62);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 10;
+            this.SaveButton.Text = "保存";
+            this.SaveButton.UseSelectable = true;
             // 
-            // metroButton3
+            // CloseButton
             // 
-            this.metroButton3.Location = new System.Drawing.Point(186, 63);
-            this.metroButton3.Name = "metroButton3";
-            this.metroButton3.Size = new System.Drawing.Size(75, 23);
-            this.metroButton3.TabIndex = 10;
-            this.metroButton3.Text = "关闭";
-            this.metroButton3.UseSelectable = true;
+            this.CloseButton.Location = new System.Drawing.Point(186, 63);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 10;
+            this.CloseButton.Text = "关闭";
+            this.CloseButton.UseSelectable = true;
             // 
-            // metroTabControl1
+            // CardTabControl
             // 
-            this.metroTabControl1.Alignment = System.Windows.Forms.TabAlignment.Left;
-            this.metroTabControl1.Location = new System.Drawing.Point(24, 92);
-            this.metroTabControl1.Multiline = true;
-            this.metroTabControl1.Name = "metroTabControl1";
-            this.metroTabControl1.Size = new System.Drawing.Size(237, 477);
-            this.metroTabControl1.TabIndex = 11;
-            this.metroTabControl1.UseSelectable = true;
+            this.CardTabControl.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.CardTabControl.Location = new System.Drawing.Point(24, 92);
+            this.CardTabControl.Multiline = true;
+            this.CardTabControl.Name = "CardTabControl";
+            this.CardTabControl.Size = new System.Drawing.Size(237, 477);
+            this.CardTabControl.TabIndex = 11;
+            this.CardTabControl.UseSelectable = true;
             // 
             // metroLabel1
             // 
@@ -143,37 +143,37 @@
             this.metroLabel4.TabIndex = 12;
             this.metroLabel4.Text = "Desc";
             // 
-            // metroComboBox1
+            // LevelBox
             // 
-            this.metroComboBox1.FormattingEnabled = true;
-            this.metroComboBox1.ItemHeight = 23;
-            this.metroComboBox1.Location = new System.Drawing.Point(394, 132);
-            this.metroComboBox1.Name = "metroComboBox1";
-            this.metroComboBox1.Size = new System.Drawing.Size(147, 29);
-            this.metroComboBox1.TabIndex = 13;
-            this.metroComboBox1.UseSelectable = true;
+            this.LevelBox.FormattingEnabled = true;
+            this.LevelBox.ItemHeight = 23;
+            this.LevelBox.Location = new System.Drawing.Point(394, 132);
+            this.LevelBox.Name = "LevelBox";
+            this.LevelBox.Size = new System.Drawing.Size(147, 29);
+            this.LevelBox.TabIndex = 13;
+            this.LevelBox.UseSelectable = true;
             // 
-            // textBox2
+            // NameBox
             // 
-            this.textBox2.Location = new System.Drawing.Point(394, 226);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(147, 21);
-            this.textBox2.TabIndex = 7;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.NameBox.Location = new System.Drawing.Point(394, 226);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(147, 21);
+            this.NameBox.TabIndex = 7;
+            this.NameBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // textBox1
+            // IDBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(394, 182);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(147, 21);
-            this.textBox1.TabIndex = 7;
+            this.IDBox.Location = new System.Drawing.Point(394, 182);
+            this.IDBox.Name = "IDBox";
+            this.IDBox.Size = new System.Drawing.Size(147, 21);
+            this.IDBox.TabIndex = 7;
             // 
-            // textBox4
+            // HPBox
             // 
-            this.textBox4.Location = new System.Drawing.Point(394, 404);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(147, 21);
-            this.textBox4.TabIndex = 7;
+            this.HPBox.Location = new System.Drawing.Point(394, 404);
+            this.HPBox.Name = "HPBox";
+            this.HPBox.Size = new System.Drawing.Size(147, 21);
+            this.HPBox.TabIndex = 7;
             // 
             // metroLabel5
             // 
@@ -184,28 +184,28 @@
             this.metroLabel5.TabIndex = 12;
             this.metroLabel5.Text = "HP";
             // 
-            // textBox5
+            // ATKBox
             // 
-            this.textBox5.Location = new System.Drawing.Point(394, 449);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(147, 21);
-            this.textBox5.TabIndex = 7;
+            this.ATKBox.Location = new System.Drawing.Point(394, 449);
+            this.ATKBox.Name = "ATKBox";
+            this.ATKBox.Size = new System.Drawing.Size(147, 21);
+            this.ATKBox.TabIndex = 7;
             // 
             // metroLabel6
             // 
             this.metroLabel6.AutoSize = true;
             this.metroLabel6.Location = new System.Drawing.Point(343, 451);
             this.metroLabel6.Name = "metroLabel6";
-            this.metroLabel6.Size = new System.Drawing.Size(32, 19);
+            this.metroLabel6.Size = new System.Drawing.Size(30, 19);
             this.metroLabel6.TabIndex = 12;
             this.metroLabel6.Text = "ATK";
             // 
-            // textBox6
+            // DEFBox
             // 
-            this.textBox6.Location = new System.Drawing.Point(394, 492);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(147, 21);
-            this.textBox6.TabIndex = 7;
+            this.DEFBox.Location = new System.Drawing.Point(394, 492);
+            this.DEFBox.Name = "DEFBox";
+            this.DEFBox.Size = new System.Drawing.Size(147, 21);
+            this.DEFBox.TabIndex = 7;
             // 
             // metroLabel7
             // 
@@ -221,7 +221,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(936, 592);
-            this.Controls.Add(this.metroComboBox1);
+            this.Controls.Add(this.LevelBox);
             this.Controls.Add(this.metroLabel4);
             this.Controls.Add(this.metroLabel3);
             this.Controls.Add(this.metroLabel7);
@@ -229,17 +229,17 @@
             this.Controls.Add(this.metroLabel5);
             this.Controls.Add(this.metroLabel2);
             this.Controls.Add(this.metroLabel1);
-            this.Controls.Add(this.metroTabControl1);
-            this.Controls.Add(this.metroButton3);
-            this.Controls.Add(this.metroButton2);
-            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.CardTabControl);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.OpenButton);
             this.Controls.Add(this.cardPictureBox);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.DescBox);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.DEFBox);
+            this.Controls.Add(this.ATKBox);
+            this.Controls.Add(this.HPBox);
+            this.Controls.Add(this.IDBox);
             this.Name = "EditorForm";
             this.Text = "至强卡牌编辑器";
             ((System.ComponentModel.ISupportInitialize)(this.cardPictureBox)).EndInit();
@@ -249,24 +249,24 @@
         }
 
         #endregion
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox DescBox;
         private System.Windows.Forms.PictureBox cardPictureBox;
-        private MetroFramework.Controls.MetroButton metroButton1;
-        private MetroFramework.Controls.MetroButton metroButton2;
-        private MetroFramework.Controls.MetroButton metroButton3;
-        private MetroFramework.Controls.MetroTabControl metroTabControl1;
+        private MetroFramework.Controls.MetroButton OpenButton;
+        private MetroFramework.Controls.MetroButton SaveButton;
+        private MetroFramework.Controls.MetroButton CloseButton;
+        private MetroFramework.Controls.MetroTabControl CardTabControl;
         private MetroFramework.Controls.MetroLabel metroLabel1;
         private MetroFramework.Controls.MetroLabel metroLabel2;
         private MetroFramework.Controls.MetroLabel metroLabel3;
         private MetroFramework.Controls.MetroLabel metroLabel4;
-        private MetroFramework.Controls.MetroComboBox metroComboBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox4;
+        private MetroFramework.Controls.MetroComboBox LevelBox;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox IDBox;
+        private System.Windows.Forms.TextBox HPBox;
         private MetroFramework.Controls.MetroLabel metroLabel5;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox ATKBox;
         private MetroFramework.Controls.MetroLabel metroLabel6;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox DEFBox;
         private MetroFramework.Controls.MetroLabel metroLabel7;
     }
 }
