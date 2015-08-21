d5704938c691cc8c01263e344523fd43daa5f234
kingwl <805037171@163.com>
2015-08-21 11:31:08 +0800

    完成保存的逻辑

--- src/Practice/CozyDungeon.RoleCardEditor/EditorForm.cs
+++ src/Practice/CozyDungeon.RoleCardEditor/EditorForm.cs
@@ -11,6 +11,11 @@ using System.Windows.Forms;
 using CozyDungeon.Game.Component.Card.Model;
 using CozyDungeon.Game.Component.Card.Enum;
 using System.Reflection;
+using System.Resources;
+using CozyDungeon.RoleCardEditor.Properties;
+using Newtonsoft.Json;
+using System.IO;
+using System.Drawing.Imaging;
 
 namespace CozyDungeon.RoleCardEditor
 {
@@ -29,9 +34,9 @@ namespace CozyDungeon.RoleCardEditor
             ResetId();
         }
 
-        private List<ListBox> CardListBoxList { get; set; } = new List<ListBox>();
-        private List<RoleCard> CardList { get; set; } = new List<RoleCard>();
-        private List<RoleCardLevel> CardLevels { get; set; } = new List<RoleCardLevel>();
+        private List<ListBox> CardListBoxList { get; set; }     = new List<ListBox>();
+        private List<RoleCard> CardList { get; set; }           = new List<RoleCard>();
+        private List<RoleCardLevel> CardLevels { get; set; }    = new List<RoleCardLevel>();
 
         private void InitTabControlPages()
         {
@@ -59,9 +64,9 @@ namespace CozyDungeon.RoleCardEditor
                        in CardLevels
                        select new KeyValuePair<string, int>(CardLevel.RoleCardLevelName(obj), (int)obj);
 
-            LevelBox.DataSource     = LevelValueList.ToList();
             LevelBox.DisplayMember  = "Key";
             LevelBox.ValueMember    = "Value";
+            LevelBox.DataSource     = LevelValueList.ToList();
         }
 
         private int InnerID = 0;
@@ -79,6 +84,7 @@ namespace CozyDungeon.RoleCardEditor
             IDBox.Text = IDMaker;
         }
 
+        private Image SelectedImage { get; set; }
         private void OpenImageButton_Click(object sender, EventArgs e)
         {
             OpenFileDialog fileDig  = new OpenFileDialog();
@@ -86,41 +92,46 @@ namespace CozyDungeon.RoleCardEditor
             if(fileDig.ShowDialog() == DialogResult.OK)
             {
                 var filename            = fileDig.FileName;
-                cardPictureBox.Image    = Image.FromFile(filename, false);
+                SelectedImage           = Image.FromFile(filename, false);
+                RefreshImage();
+                ResetId();
             }
         }
 
+        private Dictionary<int, Image> CardImageDictionary { get; set; } 
+            = new Dictionary<int, Image>();
         private void AddCardButton_Click(object sender, EventArgs e)
         {
             if(CheckInput())
             {
                 var card = new RoleCard()
                 {
-                    Level   = (RoleCardLevel)Enum.ToObject(typeof(RoleCardLevel), (int)LevelBox.SelectedValue),
+                    Level   = CardLevels[(int)LevelBox.SelectedValue],
                     Id      = int.Parse(IDBox.Text),
                     Name    = NameBox.Text,
                     Desc    = DescBox.Text,
                     ATK     = int.Parse(ATKBox.Text),
                     DEF     = int.Parse(DEFBox.Text),
                     HP      = int.Parse(HPBox.Text),
-                    Element = FiveLine.Water,
                 };
 
                 CardList.Add(card);
                 CardListBoxList[(int)LevelBox.SelectedValue].Items.Add(card.Name);
-
+                CardImageDictionary[card.Id] = cardPictureBox.Image;
                 ResetInput();
             }
         }
 
         private void ResetInput()
         {
-            ResetId();
+            LevelBox.SelectedIndex  = 0;
             NameBox.Text            = string.Empty;
             DescBox.Text            = string.Empty;
             HPBox.Text              = string.Empty;
             ATKBox.Text             = string.Empty;
             DEFBox.Text             = string.Empty;
+            SelectedImage           = null;
+            BorderImage             = null;
             cardPictureBox.Image    = null;
         }
 
@@ -130,8 +141,90 @@ namespace CozyDungeon.RoleCardEditor
             if (HPBox.Text.Length       == 0) return false;
             if (ATKBox.Text.Length      == 0) return false;
             if (DEFBox.Text.Length      == 0) return false;
-            if (cardPictureBox.Image    == null) return false;
+            if (SelectedImage           == null) return false;
             return true;
+        }
+
+        private Image BorderImage { get; set; }
+        private void LevelBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int value = (int)LevelBox.SelectedValue;
+            if ( value > 0)
+            {
+                string name = "level" + (value < 5 ? value : 5);
+                BorderImage = (Image)Resources.ResourceManager.GetObject(name);
+                RefreshImage();
+            }
+        }
+
+        private void RefreshImage()
+        {
+            var CardPictureImage = new Bitmap(270, 380);
+            using (Graphics g = Graphics.FromImage(CardPictureImage))
+            {
+                if(SelectedImage != null)
+                {
+                    g.DrawImage(SelectedImage, new Rectangle(Point.Empty, cardPictureBox.Size));
+                }
+                if(BorderImage != null)
+                {
+                    g.DrawImage(BorderImage, new Rectangle(Point.Empty, cardPictureBox.Size));
+                }
+            }
+           
+            cardPictureBox.Image = CardPictureImage;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog fileDig = new SaveFileDialog();
+            fileDig.Filter = @"json | *.json";
+            if (fileDig.ShowDialog() == DialogResult.OK)
+            {
+                var filename = new Uri(fileDig.FileName);
+                List<List<int>> savejson = new List<List<int>>();
+                for(int i = 0; i < CardLevels.Count; ++i)
+                {
+                    savejson.Add(new List<int>());
+                }
+
+                foreach(var obj in CardList)
+                {
+                    savejson[(int)obj.Level].Add(obj.Id);
+                }
+
+                using (var fs = new FileStream(filename.AbsolutePath, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    var json = JsonConvert.SerializeObject(savejson);
+                    var data = Encoding.UTF8.GetBytes(json);
+                    fs.Write(data, 0, data.Length);
+                }
+
+                var JsonDireName = Path.GetDirectoryName(filename.AbsolutePath) + @"\object\";
+                Directory.CreateDirectory(JsonDireName);
+                foreach (var obj in CardList)
+                {
+                    var json        = JsonConvert.SerializeObject(obj);
+                    var jsonname    = JsonDireName + obj.Id + ".json";
+
+                    using (var fs = new FileStream(jsonname, FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        var data = Encoding.UTF8.GetBytes(json);
+                        fs.Write(data, 0, data.Length);
+                    }
+                }
+
+                var ImageDireName = Path.GetDirectoryName(filename.AbsolutePath) + @"\image\";
+                Directory.CreateDirectory(ImageDireName);
+                foreach (var obj in CardImageDictionary)
+                {
+                    var imagename   = ImageDireName + obj.Key + ".png";
+                    using (var fs = new FileStream(imagename, FileMode.Create, FileAccess.ReadWrite))
+                    {
+                        obj.Value.Save(fs, ImageFormat.Png);
+                    }
+                }
+            }
         }
     }
 }
