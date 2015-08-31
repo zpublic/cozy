005505df7885de1c9febe637e1bbd9db3f78ca71
kingwl <805037171@163.com>
2015-08-31 21:27:53 +0800

    添加一部分关于ListView的

---
+++ src/Practice/CozyDungeon/Public/Controls/CozySampleListViewItemNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CocosSharp;
+
+namespace CozyDungeon.Public.Controls
+{
+    public class CozySampleListViewItemNode : CozySampleListViewItem
+    {
+        public CozySampleListViewItemNode(CCNode node)
+        {
+            node.AnchorPoint = CCPoint.Zero;
+            this.AddChild(node);
+            this.ContentSize = node.ContentSize;
+        }
+    }
+}
