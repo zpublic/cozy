cf1d407c12af2bbe33f36cfd9d58bd200c4f8e93
zapline <278998871@qq.com>
2015-09-01 21:06:33 +0800

    * cozybili完成基本弹幕列表

--- src/Practice/CozyBili.WpfExe/MainWindow.xaml.cs
+++ src/Practice/CozyBili.WpfExe/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace CozyBili.WpfExe
 {
@@ -25,30 +26,72 @@ namespace CozyBili.WpfExe
     {
         private LiveDanMu biliLive;
 
+        private const int _maxCapacity = 10;
+        private Queue<string> _messageQueue = new Queue<string>(_maxCapacity);
+
         public MainWindow()
         {
             InitializeComponent();
-
-            biliLive = new LiveDanMu(34083);
-            biliLive.OnlineNumChanged += OnlineNumChanged;
-            biliLive.ReceiveDanMu += ShowDanMu;
-
-            biliLive.Run();
         }
 
-        static void ShowDanMu(DanMuModel danMuModel)
+        private void ShowDanMu(DanMuModel danMuModel)
         {
-            DanmakuWindow w = new DanmakuWindow();
-            w.Show();
+            if (textBox1.Dispatcher.CheckAccess())
+            {
+                _messageQueue.Enqueue(danMuModel.UserName + ": " + danMuModel.Content);
+                textBox1.Text = string.Join("\n", _messageQueue);
+                textBox1.ScrollToEnd();
+            }
+            else
+            {
+                textBox1.Dispatcher.BeginInvoke(
+                    DispatcherPriority.Normal,
+                    new Action(() => ShowDanMu(danMuModel)));
+            }
+
+            //DanmakuWindow w = new DanmakuWindow();
+            //w.Show();
         }
 
-        static void OnlineNumChanged(int num)
+        private void OnlineNumChanged(int num)
         {
         }
 
         private void OnClose(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            biliLive.Stop();
+            if (biliLive != null)
+            {
+                biliLive.Stop();
+            }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            button.IsEnabled = false;
+            button1.IsEnabled = true;
+
+            string roomId = textBox.GetLineText(0);
+            int nId = int.Parse(roomId);
+
+            if (biliLive == null)
+            {
+                biliLive = new LiveDanMu(nId);
+                biliLive.OnlineNumChanged += OnlineNumChanged;
+                biliLive.ReceiveDanMu += ShowDanMu;
+                biliLive.Run();
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            button1.IsEnabled = false;
+            button.IsEnabled = true;
+
+            if (biliLive != null)
+            {
+                biliLive.Stop();
+                biliLive = null;
+            }
         }
     }
 }
