59056f14b8d31e900ddbe8d7c5208631072978f2
kingwl <805037171@163.com>
2015-09-01 13:48:52 +0800

    整理功能

--- src/Practice/CozyDict.Core/MouseUtil.cs
+++ src/Practice/CozyDict.Core/MouseUtil.cs
@@ -7,17 +7,10 @@ using System.Runtime.InteropServices;
 
 namespace CozyDict.Core
 {
-    public class MouseUtil
+    public static class MouseUtil
     {
         public delegate IntPtr MouseHookCallback(int code, UIntPtr wParam, IntPtr lParam);
 
-        public delegate bool TextOutACallback(IntPtr hdc, int x, int y, IntPtr lpString, int c);
-
-        public delegate bool TextOutWCallback(IntPtr hdc, int x, int y, IntPtr lpString, int c);
-
-        public delegate bool ExtTextOutACallback(IntPtr hdc, int x, int y, uint optionsm, IntPtr rect, IntPtr lpString, uint c, IntPtr dx);
-
-        public delegate bool ExtTextOutWCallback(IntPtr hdc, int x, int y, uint optionsm, IntPtr rect, IntPtr lpString, uint c, IntPtr dx);
 
         [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool SetMouseHook(MouseHookCallback callback);
@@ -28,25 +21,6 @@ namespace CozyDict.Core
         [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool InvalidateMouseWindow(int x, int y);
 
-        [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void ResetAPIHookCallback();
-
-        [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern void InitHookEnv();
-
-        [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern bool SetTextOutAHook(TextOutACallback callback);
-
-        [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern bool SetTextOutWHook(TextOutWCallback callback);
-
-        [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern bool SetExtTextOutAHook(ExtTextOutACallback callback);
-
-        [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern bool SetExtTextOutWHook(ExtTextOutWCallback callback);
-
-        [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
-        public static extern bool UnsetAllHook();
+        
     }
 }
---
+++ src/Practice/CozyDict.Core/OutputUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace CozyDict.Core
+{
+    public static class OutputUtil
+    {
+        public delegate bool TextOutACallback(IntPtr hdc, int x, int y, IntPtr lpString, int c);
+
+        public delegate bool TextOutWCallback(IntPtr hdc, int x, int y, IntPtr lpString, int c);
+
+        public delegate bool ExtTextOutACallback(IntPtr hdc, int x, int y, uint optionsm, IntPtr rect, IntPtr lpString, uint c, IntPtr dx);
+
+        public delegate bool ExtTextOutWCallback(IntPtr hdc, int x, int y, uint optionsm, IntPtr rect, IntPtr lpString, uint c, IntPtr dx);
+
+        [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void ResetAPIHookCallback();
+
+        [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void InitHookEnv();
+
+        [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool SetTextOutAHook(TextOutACallback callback);
+
+        [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool SetTextOutWHook(TextOutWCallback callback);
+
+        [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool SetExtTextOutAHook(ExtTextOutACallback callback);
+
+        [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool SetExtTextOutWHook(ExtTextOutWCallback callback);
+
+        [DllImport(@"CozyDict.Base.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool UnsetAllHook();
+    }
+}
