11263f2bf167936e1d52d221af14ffe5239eb70e
Max <763305825@qq.com>
2015-09-01 11:50:54 +0800

    成功运行Nancy，基于Asp.net做host

---
+++ src/Practice/CozyMarkDonw.Mvc/Modlues/HomeModlue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace CozyMarkDonw.Mvc.Modlues {
+
+    public class HomeModlue : NancyModule {
+
+        public HomeModlue() {
+            Get["/"] = x => {
+                return View["/Home"];
+            };
+        }
+    }
+}
