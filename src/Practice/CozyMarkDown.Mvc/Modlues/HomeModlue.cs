811fdb865eb1d69c9a4e91cc41bad998061f93f2
Max <763305825@qq.com>
2015-09-01 18:00:26 +0800

    完成主页面

--- src/Practice/CozyMarkDown.Mvc/Modlues/HomeModlue.cs
+++ src/Practice/CozyMarkDown.Mvc/Modlues/HomeModlue.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Nancy;
+﻿using Nancy;
+using CozyMarkDonw.Mvc.Models;
 
 namespace CozyMarkDown.Mvc.Modlues {
 
     public class HomeModlue : NancyModule {
 
         public HomeModlue() {
-            
+
 
             Get["/"] = x => {
-                return View["/Index"];
+                return View["/Home/Index", ArticleModel.TestData()];
             };
         }
     }
