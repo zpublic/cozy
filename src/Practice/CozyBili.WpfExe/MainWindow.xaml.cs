3d0f53c6059b6bea1b3dfac43e5cf3c5aa4197da
zapline <278998871@qq.com>
2015-09-01 14:33:28 +0800

    * 准备开写wpf版弹幕鸡

--- src/Practice/CozyBili.WpfExe/MainWindow.xaml.cs
+++ src/Practice/CozyBili.WpfExe/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CozyBili.Core;
+using CozyBili.Core.Models;
+using CozyBili.Danmaku.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +23,32 @@ namespace CozyBili.WpfExe
     /// </summary>
     public partial class MainWindow : Window
     {
+        private LiveDanMu biliLive;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            biliLive = new LiveDanMu(34083);
+            biliLive.OnlineNumChanged += OnlineNumChanged;
+            biliLive.ReceiveDanMu += ShowDanMu;
+
+            biliLive.Run();
+        }
+
+        static void ShowDanMu(DanMuModel danMuModel)
+        {
+            DanmakuWindow w = new DanmakuWindow();
+            w.Show();
+        }
+
+        static void OnlineNumChanged(int num)
+        {
+        }
+
+        private void OnClose(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            biliLive.Stop();
         }
     }
 }
