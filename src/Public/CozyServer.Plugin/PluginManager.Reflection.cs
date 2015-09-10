8af7463b9ad866ee0f3d7f0100fef8e2a2c5e2a7
kingwl <805037171@163.com>
2015-09-10 23:45:04 +0800

    继续添加Server

---
+++ src/Public/CozyServer.Plugin/PluginManager.Reflection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace CozyServer.Plugin
+{
+    public partial class PluginManager
+    {
+        public void AddPlugin(IPlugin plugin)
+        {
+            PluginSet.Add(plugin);
+        }
+
+        public bool TryAddPluginWithFilename(string filename)
+        {
+            bool result = false;
+
+            if(filename != null && filename.Length > 0)
+            {
+                if(Path.GetExtension(filename) == @"dll")
+                {
+                    Assembly assembly = Assembly.LoadFrom(filename);
+                    if(assembly != null)
+                    {
+                        var pluginTypes = assembly.GetTypes();
+                        foreach(var obj in pluginTypes)
+                        {
+                            if(typeof(IPlugin).IsAssignableFrom(obj))
+                            {
+                                var plugin = (IPlugin)Activator.CreateInstance(obj);
+                                AddPlugin(plugin);
+                                result = true;
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
