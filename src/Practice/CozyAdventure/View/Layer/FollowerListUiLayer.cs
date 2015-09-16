26e8a700ae10c83ca9181e5019c6c83b9858d6d6
BZPhaha <783535094@qq.com>
2015-09-16 20:49:24 +0800

    添加佣兵界面

---
+++ src/Practice/CozyAdventure/View/Layer/FollowerListUiLayer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using CocosSharp;
+using CozyAdventure.View.Layer;
+using CozyAdventure.Public.Controls;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CozyAdventure.View.Layer
+{
+    public class FollowerListUiLayer : CCLayer
+    {
+        public FollowerListUiLayer()
+        {
+            var title = new CCLabel("冒险与编程", "微软雅黑", 72)
+            {
+                Position = new CCPoint(400, 320),
+                Color = CCColor3B.Yellow
+            };
+            AddChild(title, 100);
+
+            //var listlable = new CCLabel("佣兵列表", "微软雅黑", 14)
+            //{
+            //    Position = new CCPoint(92, 20),
+            //    Color = CCColor3B.Black
+            //};
+            //AddChild(listlable, 100);
+            //for (int i = 1; i <= 12; i++)
+            //{
+            //    for (int a = 92; a <= 543; a += 148)
+            //    {
+            //        for (int b = 52; b <= 289; b += 119)
+            //        {
+            //            var Follower = new BaseButton(a, b, 120, 100)
+            //            {
+            //                Text = "佣兵" + i,
+            //                FontSize = 14
+            //            };
+            //            AddChild(Follower, 100);
+            //        }
+            //    }
+            //}
+            //int c=20;
+            //int d=30;
+            //var AllFollower = new CCLabel("总数" + c + "/" + d, "微软雅黑", 14)
+            //{
+            //    Position = new CCPoint(92, 420),
+            //    Color = CCColor3B.White,
+            //};
+            //AddChild(AllFollower, 100);
+            //int e = 20;
+            //int f = 30;
+            //var PageNumber = new CCLabel(e + "/" + f, "微软雅黑", 14)
+            //{
+            //    Position = new CCPoint(389, 420),
+            //    Color = CCColor3B.Black
+            //};
+            //AddChild(PageNumber, 100);
+            //var LastPage = new BaseButton(473,410,78,36)
+            //{
+            //    Text="上一页",
+            //    FontSize=14
+            //};
+            //AddChild(LastPage,100);
+            //var NextPage = new BaseButton(585, 410, 78, 36)
+            //{
+            //    Text = "下一页",
+            //    FontSize = 14
+            //};
+            //AddChild(NextPage, 100);
+        }
+    }
+}
---
+++ src/Practice/CozyAdventure/View/Scene/FollowerListScene.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CocosSharp;
+using System.Collections.Generic;
+using CozyAdventure.Public.Controls;
+using CozyAdventure.View.Layer;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyAdventure.View.Scene
+{
+    public class FollowerListScene : CCScene
+    {
+        public FollowerListScene() : base(AppDelegate.SharedWindow)
+        {
+            var ui = new FollowerListUiLayer();
+            AddChild(ui);
+
+            //var bg = new BackgroundLayer(@"pic\1.png");
+            //AddChild(bg);
+            
+        }
+    }
+}
