b88c1fb7860c4157e52b74daee038c0ececda852
kingwl <805037171@163.com>
2015-09-28 22:08:03 +0800

    review campLayer

--- src/Practice/CozyAdventure/View/Layer/CampUiLayer.cs
+++ src/Practice/CozyAdventure/View/Layer/CampUiLayer.cs
@@ -9,42 +9,43 @@ using System.Threading.Tasks;
 
 namespace CozyAdventure.View.Layer
 {
-   public class CampUiLayer : CCLayer
+    public class CampUiLayer : CCLayer
     {
-            int Fighting=0;
-            int Gold=0;
-            int Exper =0;
+        int Fighting    = 0;
+        int Gold        = 0;
+        int Exper       = 0;
         public CampUiLayer()
         {
-            
-            var edit = new CCLabel("战斗力:"+Fighting+"金币"+Gold+"经验"+Exper, "微软雅黑", 22)
+
+            var edit = new CCLabel("战斗力:" + Fighting + "金币" + Gold + "经验" + Exper, "微软雅黑", 22)
             {
-                Position = new CCPoint(100, 120),
-                Color = CCColor3B.Blue
+                Position    = new CCPoint(100, 120),
+                AnchorPoint = CCPoint.Zero,
+                Color       = CCColor3B.Blue
             };
             AddChild(edit, 100);
             var Goon = new CozySampleButton(631, 86, 78, 36)
             {
-                Text = "继续冒险",
-                FontSize = 14
+                Text        = "继续冒险",
+                FontSize    = 14
             };
             AddChild(Goon, 100);
             var MercMange = new CozySampleButton(631, 160, 78, 36)
             {
-                Text = "佣兵管理",
-                FontSize = 14
+                Text        = "佣兵管理",
+                FontSize    = 14
             };
             AddChild(MercMange, 100);
             var MyGoods = new CozySampleButton(631, 227, 78, 36)
             {
-                Text = "我的物品",
-                FontSize = 14
+                Text        = "我的物品",
+                FontSize    = 14
             };
             AddChild(MyGoods, 100);
             var MyFriends = new CozySampleButton(631, 299, 78, 36)
             {
-                Text = "我的好友",
-                FontSize = 14
+                Text        = "我的好友",
+                FontSize    = 14
             };
             AddChild(MyFriends, 100);
         }
