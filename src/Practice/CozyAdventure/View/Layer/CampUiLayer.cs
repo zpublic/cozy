7a75a27d2cd4e2051629d12b8d2126e25f0d7b16
kingwl <805037171@163.com>
2015-09-29 18:34:05 +0800

    添加Camp对触摸的响应

--- src/Practice/CozyAdventure/View/Layer/CampUiLayer.cs
+++ src/Practice/CozyAdventure/View/Layer/CampUiLayer.cs
@@ -1,55 +1,80 @@
 ﻿using System;
 using CocosSharp;
 using CozyAdventure.Public.Controls;
-using CozyAdventure.View.Layer;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CozyAdventure.Game.Object;
 using CozyAdventure.Game.Logic;
+using CozyAdventure.View.Scene;
 
 namespace CozyAdventure.View.Layer
 {
     public class CampUiLayer : CCLayer
     {
+        private CCLabel EditNode { get; set; }
+
         public CampUiLayer()
         {
-            
-            var Fighting    = FollowerCollectLogic.GetAttack(PlayerObject.Instance.Self.FightFollower);
-            var Money       = PlayerObject.Instance.Self.Money;
-            var Exp         = PlayerObject.Instance.Self.Exp;
-            var edit = new CCLabel(string.Format("战斗力: + {0} +  金币 + {1} + 经验 + {2}", Fighting, Money, Exp), "微软雅黑", 22)
+            EditNode = new CCLabel("", "微软雅黑", 22)
             {
                 Position    = new CCPoint(100, 120),
                 AnchorPoint = CCPoint.Zero,
                 Color       = CCColor3B.Blue
             };
-            AddChild(edit, 100);
+            AddChild(EditNode, 100);
+
             var Goon = new CozySampleButton(631, 86, 78, 36)
             {
                 Text        = "继续冒险",
-                FontSize    = 14
+                FontSize    = 14,
+                OnClick     = () =>
+                {
+                    AppDelegate.SharedWindow.DefaultDirector.ReplaceScene(new FarmScene(2));
+                }
             };
+            this.AddEventListener(Goon.EventListener);
             AddChild(Goon, 100);
+
             var MercMange = new CozySampleButton(631, 160, 78, 36)
             {
                 Text        = "佣兵管理",
-                FontSize    = 14
+                FontSize    = 14,
+                OnClick     = () =>
+                {
+                    AppDelegate.SharedWindow.DefaultDirector.ReplaceScene(new FollowerListScene());
+                }
             };
+            this.AddEventListener(MercMange.EventListener);
             AddChild(MercMange, 100);
+
             var MyGoods = new CozySampleButton(631, 227, 78, 36)
             {
                 Text        = "我的物品",
                 FontSize    = 14
             };
+            this.AddEventListener(MyGoods.EventListener);
             AddChild(MyGoods, 100);
+
             var MyFriends = new CozySampleButton(631, 299, 78, 36)
             {
                 Text        = "我的好友",
                 FontSize    = 14
             };
+            this.AddEventListener(MyFriends.EventListener);
             AddChild(MyFriends, 100);
+
+            RefreshPlayerInfo();
+        }
+
+        private void RefreshPlayerInfo()
+        {
+            var Fighting    = FollowerCollectLogic.GetAttack(PlayerObject.Instance.Self.FightFollower);
+            var Money       = PlayerObject.Instance.Self.Money;
+            var Exp         = PlayerObject.Instance.Self.Exp;
+
+            EditNode.Text = string.Format("战斗力: + {0} +  金币 + {1} + 经验 + {2}", Fighting, Money, Exp);
         }
     }
 }
