6d0943950ffa4f7b548f2f9fdcdfa94e1880d869
kingwl <805037171@163.com>
2015-10-20 18:50:55 +0800

    添加回显

---
+++ src/Practice/CozyWiki/Setting.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyWiki
+{
+    public class Setting
+    {
+        public string RootDir { get; set; } = @".\";
+
+        public int Port { get; set; } = 80;
+
+        private static Setting instance { get; set; } = new Setting();
+        public static Setting Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void Init()
+        {
+            if (File.Exists("setting.json"))
+            {
+                using (var fs = new FileStream("setting.json", FileMode.Open, FileAccess.Read))
+                {
+                    using (var reader = new StreamReader(fs))
+                    {
+                        var context = reader.ReadToEnd();
+                        instance = JsonConvert.DeserializeObject<Setting>(context);
+                    }
+                }
+            }
+            else
+            {
+                instance = new Setting();
+
+                using (var fs = new FileStream("setting.json", FileMode.Create, FileAccess.ReadWrite))
+                {
+                    using (var writer = new StreamWriter(fs))
+                    {
+                        var context = JsonConvert.SerializeObject(instance);
+                        writer.Write(context);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Practice/CozyWiki/WikiModule.cs
@@ -0,0 +1,81 @@
+﻿using Nancy;
+using CommonMark;
+using Nancy.Hosting.Self;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CozyWiki
+{
+    public class WikiModule : NancyModule
+    {
+        public WikiModule()
+        {
+            Get["/"] = x =>
+            {
+                return "hello world!";
+            };
+
+            Get["/p/{path}"] = (x) =>
+            {
+                var p = Path.Combine(Setting.Instance.RootDir, x.path + ".md");
+                if (File.Exists(p))
+                {
+                    using (var fs = new FileStream(p, FileMode.Open, FileAccess.Read))
+                    {
+                        using (var reader = new StreamReader(fs, Encoding.UTF8))
+                        {
+                            var context = reader.ReadToEnd();
+                            return CommonMarkConverter.Convert(context);
+                        }
+                    }
+                }
+                else
+                {
+                    return Get404Page();
+                }
+            };
+            Get["/m/{path}"] = x =>
+            {
+                var p = Path.Combine(Setting.Instance.RootDir, x.path + ".md");
+                if (File.Exists(p))
+                {
+                    using (var fs = new FileStream(p, FileMode.Open, FileAccess.Read))
+                    {
+                        using (var reader = new StreamReader(fs, Encoding.UTF8))
+                        {
+                            var context = reader.ReadToEnd();
+                            return context;
+                        }
+                    }
+                }
+                else
+                {
+                    return Get404Page();
+                }
+            };
+        }
+
+        public string Get404Page()
+        {
+            var p = Path.Combine(Setting.Instance.RootDir, "404.html");
+            if (File.Exists(p))
+            {
+                using (var fs = new FileStream(p, FileMode.Open, FileAccess.Read))
+                {
+                    using (var reader = new StreamReader(fs))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+            else
+            {
+                return "404 fot found";
+            }
+        }
+    }
+}
