99328b45dc7144041edfe4bf4a9429d8572b443a
Max <763305825@qq.com>
2015-10-20 18:04:15 +0800

    create nancy project

---
+++ src/Practice/CozyMarkdown.WebStie/Module/Home.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace CozyMarkdown.WebStie.Module {
+
+    public class Home : NancyModule {
+
+        public Home() {
+
+            Get["/"] = x => {
+                return View["Home", new { Message = "hello,CozyMarkdown! " }];
+            };
+
+        }
+    }
+}
---
+++ src/Practice/CozyMarkdown.WebStie/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Nancy;
+using Nancy.Hosting.Self;
+using System.Diagnostics;
+
+namespace CozyMarkdown.WebStie {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            var baseUrl = "http://localhost:8888/";
+
+            HostConfiguration hostconfig = new HostConfiguration() {
+                UrlReservations = new UrlReservations { CreateAutomatically = true }
+            };
+
+            using (var host = new NancyHost(new Url(baseUrl), new DefaultNancyBootstrapper(), hostconfig)) {
+                host.Start();
+                Process.Start(baseUrl);
+                Console.WriteLine("CozyMarkdown is runing , Press enter to stop");
+                Console.ReadKey();
+
+            }
+
+        }
+    }
+}
