dd3701e094edcb528b72df7609624cceeab445cf
kingwl <805037171@163.com>
2015-10-28 23:42:38 +0800

    微调TabControl的自绘

--- src/Practice/CozyPixel/Controls/VerticalTabControl.cs
+++ src/Practice/CozyPixel/Controls/VerticalTabControl.cs
@@ -10,8 +10,16 @@ namespace CozyPixel.Controls
 {
     public class VerticalTabControl : TabControl
     {
+        public StringFormat ItemTextFormat { get; set; }
+
         public VerticalTabControl()
         {
+            ItemTextFormat = new StringFormat()
+            {
+                Alignment       = StringAlignment.Center,
+                LineAlignment   = StringAlignment.Center,
+            };
+
             DrawItem += On_TabControl_DrawItem;
 
             DrawMode = TabDrawMode.OwnerDrawFixed;
@@ -23,7 +31,7 @@ namespace CozyPixel.Controls
             var font        = e.Font;
             var brush       = new SolidBrush(Color.Black);
             var tabTextArea = (RectangleF)GetTabRect(e.Index);
-            g.DrawString(Controls[e.Index].Text, font, brush, tabTextArea);
+            g.DrawString(Controls[e.Index].Text, font, brush, tabTextArea, ItemTextFormat);
         }
     }
 }
