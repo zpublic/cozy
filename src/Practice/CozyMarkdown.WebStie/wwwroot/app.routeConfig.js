d0c7b3ebd43f391d2e2f63c87115ea776d99fa72
Max <763305825@qq.com>
2015-11-02 18:13:35 +0800

    对require和angular进行简单配置

---
+++ src/Practice/CozyMarkdown.WebStie/wwwroot/app.routeConfig.js
@@ -0,0 +1,12 @@
+﻿define([], function () {
+    return {
+        defaultRoute: '/welcome',
+        routes: {
+            '/welcome': {
+                templateUrl: 'components/shared/welcomeView.html',
+                controller: 'WelcomeController',
+                dependencies: ['components/shared/welcomeController']
+            }
+        }
+    };
+});
