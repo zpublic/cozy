cae3da98a61934978a78633bb0b6ff32067c01e2
Max <763305825@qq.com>
2015-11-03 16:58:52 +0800

    完成core并测试通过

--- src/Practice/CozyBored.Client.Cheat/Program.cs
+++ src/Practice/CozyBored.Client.Cheat/Program.cs
@@ -5,17 +5,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CozyBored.Client.Cheat
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string name = Console.ReadLine();
-            int time = int.Parse(Console.ReadLine());
-            BoredApi.Save(name, time);
-            BoredApi.QueryRank();
-            Console.WriteLine(name + ":" + time);
+namespace CozyBored.Client.Cheat {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            //string name = Console.ReadLine();
+            //int time = int.Parse(Console.ReadLine());
+            //BoredApi.Save(new Core.Model.BoredModel {
+            //    time = 1,
+            //    ver = "1",
+            //    name = "cozy"
+            //});
+            //BoredApi.QueryRank("1");
+            //BoredApi.GetRank("1", 1);
+            //Console.WriteLine(name + ":" + time);
         }
     }
 }
---
+++ src/Practice/CozyBored.Client.Core/Model/BoredModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CozyBored.Client.Core.Model {
+
+    public class BoredModel {
+        
+        public int time { get; set; }
+        public string ver { get; set; }
+        public string name { get; set; }
+    }
+}
