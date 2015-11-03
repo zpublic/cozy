0400c597fe60120195ca0ecb9d390ff1278b5d27
Max <763305825@qq.com>
2015-11-03 10:57:59 +0800

    添加排行榜model

---
+++ src/Practice/CozyBored.Server/Models/RankModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CozyBored.Server.Models {
+
+    public class RankModel {
+
+        public DateTime time { get; set; }
+        public string ver { get; set; }
+        public string name { get; set; }
+    }
+}
