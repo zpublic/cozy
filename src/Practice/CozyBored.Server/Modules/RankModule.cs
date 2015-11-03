ce88c0748606708df30b1f8807b6de75ad4ff961
Max <763305825@qq.com>
2015-11-03 11:05:48 +0800

    定义api接口

---
+++ src/Practice/CozyBored.Server/Modules/RankModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace CozyBored.Server.Modules {
+
+    public class RankModule : NancyModule {
+
+        public RankModule() {
+
+            Get["query-rank"] = param => {
+                throw new NotImplementedException();
+            };
+
+            Post["save"] = param => {
+                throw new NotImplementedException();
+            };
+        }
+
+    }
+}
