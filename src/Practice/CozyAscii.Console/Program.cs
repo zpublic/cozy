595fce2644f476ce12f3de3e5986d9605fdf0c66
zapline <278998871@qq.com>
2015-11-04 15:16:40 +0800

    + 新坑 CozyAscii

---
+++ src/Practice/CozyAscii.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyAscii.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] chars = { '@', 'w', '#', '$', 'k', 'd', 't', 'j', 'i', '.', ' ' };
+            Bitmap b = new Bitmap(@"g:\aqa.bmp");
+            for (int y = 0; y < b.Height; ++y)
+            {
+                for (int x = 0; x < b.Width; ++x)
+                {
+                    System.Console.Write(chars[(b.GetPixel(x, y).R + b.GetPixel(x, y).G + b.GetPixel(x, y).B) / 3 / 25]);
+                }
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
