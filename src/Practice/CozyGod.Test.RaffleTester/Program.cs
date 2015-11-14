23b6995a61769bdf844c5b3eb1ed3a2d775f8d6a
torai <douglesmi@163.com>
2015-11-14 11:40:49 +0800

    [+]添加craft和raffle测试代码

--- src/Practice/CozyGod.Test.RaffleTester/Program.cs
+++ src/Practice/CozyGod.Test.RaffleTester/Program.cs
@@ -2,17 +2,33 @@
 using CozyGod.Game.Interface;
 using CozyGod.Model;
 
+
 namespace CozyGod.Test.RaffleTester
 {
     class Program
     {
         static void Main(string[] args)
         {
+            const int drawTestNumber = 100;
             ICozyGodEngine engine = new CozyGodEngine();
             engine.Init();
 
-            IRaffle i = engine.GetRaffle();
-            Card c = i.Draw();
+            Card[] pentaDrawTest;
+            Card drawTest;
+            IRaffle iRaffle = engine.GetRaffle();
+
+            for (int n = 0; n < drawTestNumber; n++)
+            {
+                pentaDrawTest = iRaffle.PentaDraw();
+                drawTest = iRaffle.Draw(2);
+                int index = 0;
+                while (pentaDrawTest[index] != null)
+                {
+                    System.Console.Write("card name : {0}, card level : {1}", pentaDrawTest[index].Name, pentaDrawTest[index].Level);
+                }
+                System.Console.Write("\n");
+                System.Console.WriteLine("card name : {0}, card level : {1}", drawTest.Name, drawTest.Level);
+            }
         }
     }
 }
