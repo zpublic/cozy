932e04aac6609df02f13f8f1692f3a7fdfd6cf84
kingwl <805037171@163.com>
2015-11-14 22:39:05 +0800

    完成生成器

--- src/Practice/CozyGod.Tool.CardGenerator/MainForm.Designer.cs
+++ src/Practice/CozyGod.Tool.CardGenerator/MainForm.Designer.cs
@@ -28,87 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.SelectImagePathButton = new System.Windows.Forms.Button();
+            this.SelectCraftTableButton = new System.Windows.Forms.Button();
+            this.SelectTranslateTableButton = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
-            this.button4 = new System.Windows.Forms.Button();
+            this.GenButton = new System.Windows.Forms.Button();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SelectOutputPathButton = new System.Windows.Forms.Button();
+            this.ImagePathBox = new System.Windows.Forms.TextBox();
+            this.CraftTableBox = new System.Windows.Forms.TextBox();
+            this.TranslateTableBox = new System.Windows.Forms.TextBox();
+            this.OutputPathBox = new System.Windows.Forms.TextBox();
+            this.SelectLevelBackgroundButton = new System.Windows.Forms.Button();
+            this.SelectBorderButton = new System.Windows.Forms.Button();
+            this.LevelBackgroundBox = new System.Windows.Forms.TextBox();
+            this.BorderBackgroundBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // SelectImagePathButton
             // 
-            this.button1.Location = new System.Drawing.Point(94, 44);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "选择图片路径";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.SelectImagePathButton.Location = new System.Drawing.Point(12, 21);
+            this.SelectImagePathButton.Name = "SelectImagePathButton";
+            this.SelectImagePathButton.Size = new System.Drawing.Size(100, 23);
+            this.SelectImagePathButton.TabIndex = 0;
+            this.SelectImagePathButton.Text = "选择图片路径";
+            this.SelectImagePathButton.UseVisualStyleBackColor = true;
+            this.SelectImagePathButton.Click += new System.EventHandler(this.SelectImageDireButton_Click);
             // 
-            // button2
+            // SelectCraftTableButton
             // 
-            this.button2.Location = new System.Drawing.Point(94, 87);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "选择合成表";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.SelectCraftTableButton.Location = new System.Drawing.Point(12, 50);
+            this.SelectCraftTableButton.Name = "SelectCraftTableButton";
+            this.SelectCraftTableButton.Size = new System.Drawing.Size(100, 23);
+            this.SelectCraftTableButton.TabIndex = 1;
+            this.SelectCraftTableButton.Text = "选择合成表";
+            this.SelectCraftTableButton.UseVisualStyleBackColor = true;
+            this.SelectCraftTableButton.Click += new System.EventHandler(this.SelectCraftTablePathButton_Click);
             // 
-            // button3
+            // SelectTranslateTableButton
             // 
-            this.button3.Location = new System.Drawing.Point(94, 140);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "选择翻译表";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.SelectTranslateTableButton.Location = new System.Drawing.Point(12, 79);
+            this.SelectTranslateTableButton.Name = "SelectTranslateTableButton";
+            this.SelectTranslateTableButton.Size = new System.Drawing.Size(100, 23);
+            this.SelectTranslateTableButton.TabIndex = 2;
+            this.SelectTranslateTableButton.Text = "选择翻译表";
+            this.SelectTranslateTableButton.UseVisualStyleBackColor = true;
+            this.SelectTranslateTableButton.Click += new System.EventHandler(this.SelectTranslateTablePathButton_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
-            // button4
+            // GenButton
             // 
-            this.button4.Location = new System.Drawing.Point(94, 191);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(100, 23);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "生成";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.GenButton.Location = new System.Drawing.Point(12, 196);
+            this.GenButton.Name = "GenButton";
+            this.GenButton.Size = new System.Drawing.Size(100, 23);
+            this.GenButton.TabIndex = 3;
+            this.GenButton.Text = "生成";
+            this.GenButton.UseVisualStyleBackColor = true;
+            this.GenButton.Click += new System.EventHandler(this.GenButton_Click);
             // 
             // saveFileDialog1
             // 
             this.saveFileDialog1.Filter = "*.json | *.json";
             // 
+            // SelectOutputPathButton
+            // 
+            this.SelectOutputPathButton.Location = new System.Drawing.Point(12, 108);
+            this.SelectOutputPathButton.Name = "SelectOutputPathButton";
+            this.SelectOutputPathButton.Size = new System.Drawing.Size(100, 23);
+            this.SelectOutputPathButton.TabIndex = 4;
+            this.SelectOutputPathButton.Text = "选择输出目录";
+            this.SelectOutputPathButton.UseVisualStyleBackColor = true;
+            this.SelectOutputPathButton.Click += new System.EventHandler(this.SelectOutputPathButton_Click);
+            // 
+            // ImagePathBox
+            // 
+            this.ImagePathBox.Location = new System.Drawing.Point(145, 21);
+            this.ImagePathBox.Name = "ImagePathBox";
+            this.ImagePathBox.ReadOnly = true;
+            this.ImagePathBox.Size = new System.Drawing.Size(100, 21);
+            this.ImagePathBox.TabIndex = 5;
+            // 
+            // CraftTableBox
+            // 
+            this.CraftTableBox.Location = new System.Drawing.Point(145, 52);
+            this.CraftTableBox.Name = "CraftTableBox";
+            this.CraftTableBox.ReadOnly = true;
+            this.CraftTableBox.Size = new System.Drawing.Size(100, 21);
+            this.CraftTableBox.TabIndex = 6;
+            // 
+            // TranslateTableBox
+            // 
+            this.TranslateTableBox.Location = new System.Drawing.Point(145, 81);
+            this.TranslateTableBox.Name = "TranslateTableBox";
+            this.TranslateTableBox.ReadOnly = true;
+            this.TranslateTableBox.Size = new System.Drawing.Size(100, 21);
+            this.TranslateTableBox.TabIndex = 7;
+            // 
+            // OutputPathBox
+            // 
+            this.OutputPathBox.Location = new System.Drawing.Point(145, 110);
+            this.OutputPathBox.Name = "OutputPathBox";
+            this.OutputPathBox.ReadOnly = true;
+            this.OutputPathBox.Size = new System.Drawing.Size(100, 21);
+            this.OutputPathBox.TabIndex = 8;
+            // 
+            // SelectLevelBackgroundButton
+            // 
+            this.SelectLevelBackgroundButton.Location = new System.Drawing.Point(12, 137);
+            this.SelectLevelBackgroundButton.Name = "SelectLevelBackgroundButton";
+            this.SelectLevelBackgroundButton.Size = new System.Drawing.Size(100, 23);
+            this.SelectLevelBackgroundButton.TabIndex = 9;
+            this.SelectLevelBackgroundButton.Text = "选择Level背景";
+            this.SelectLevelBackgroundButton.UseVisualStyleBackColor = true;
+            this.SelectLevelBackgroundButton.Click += new System.EventHandler(this.SelectLevelBackgroundButton_Click);
+            // 
+            // SelectBorderButton
+            // 
+            this.SelectBorderButton.Location = new System.Drawing.Point(12, 167);
+            this.SelectBorderButton.Name = "SelectBorderButton";
+            this.SelectBorderButton.Size = new System.Drawing.Size(100, 23);
+            this.SelectBorderButton.TabIndex = 10;
+            this.SelectBorderButton.Text = "设置边框";
+            this.SelectBorderButton.UseVisualStyleBackColor = true;
+            this.SelectBorderButton.Click += new System.EventHandler(this.SelectBorderButton_Click);
+            // 
+            // LevelBackgroundBox
+            // 
+            this.LevelBackgroundBox.Location = new System.Drawing.Point(145, 138);
+            this.LevelBackgroundBox.Name = "LevelBackgroundBox";
+            this.LevelBackgroundBox.ReadOnly = true;
+            this.LevelBackgroundBox.Size = new System.Drawing.Size(100, 21);
+            this.LevelBackgroundBox.TabIndex = 11;
+            // 
+            // BorderBackgroundBox
+            // 
+            this.BorderBackgroundBox.Location = new System.Drawing.Point(145, 166);
+            this.BorderBackgroundBox.Name = "BorderBackgroundBox";
+            this.BorderBackgroundBox.ReadOnly = true;
+            this.BorderBackgroundBox.Size = new System.Drawing.Size(100, 21);
+            this.BorderBackgroundBox.TabIndex = 12;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(314, 261);
+            this.Controls.Add(this.BorderBackgroundBox);
+            this.Controls.Add(this.LevelBackgroundBox);
+            this.Controls.Add(this.SelectBorderButton);
+            this.Controls.Add(this.SelectLevelBackgroundButton);
+            this.Controls.Add(this.OutputPathBox);
+            this.Controls.Add(this.TranslateTableBox);
+            this.Controls.Add(this.CraftTableBox);
+            this.Controls.Add(this.ImagePathBox);
+            this.Controls.Add(this.SelectOutputPathButton);
+            this.Controls.Add(this.GenButton);
+            this.Controls.Add(this.SelectTranslateTableButton);
+            this.Controls.Add(this.SelectCraftTableButton);
+            this.Controls.Add(this.SelectImagePathButton);
             this.Name = "MainForm";
             this.Text = "CardGenerator";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button SelectImagePathButton;
+        private System.Windows.Forms.Button SelectCraftTableButton;
+        private System.Windows.Forms.Button SelectTranslateTableButton;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button GenButton;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button SelectOutputPathButton;
+        private System.Windows.Forms.TextBox ImagePathBox;
+        private System.Windows.Forms.TextBox CraftTableBox;
+        private System.Windows.Forms.TextBox TranslateTableBox;
+        private System.Windows.Forms.TextBox OutputPathBox;
+        private System.Windows.Forms.Button SelectLevelBackgroundButton;
+        private System.Windows.Forms.Button SelectBorderButton;
+        private System.Windows.Forms.TextBox LevelBackgroundBox;
+        private System.Windows.Forms.TextBox BorderBackgroundBox;
     }
 }
 
