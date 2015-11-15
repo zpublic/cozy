4a24a1dd8d0d326d57ccd3b8213c55e0a7ae7037
kingwl <805037171@163.com>
2015-11-15 20:33:22 +0800

    添加CardLibTester 并改.net版本为4.5

---
+++ src/Practice/CozyGod.Test.CardLibraryTester/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CozyGod.Test.CardLibraryTester
+{
+    static class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
