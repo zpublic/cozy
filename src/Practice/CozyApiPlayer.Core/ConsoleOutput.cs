62cca1d6b541dba17b2f309258c151269c1914de
zapline <278998871@qq.com>
2015-11-23 10:47:36 +0800

    + 开坑CozyApiPlayer

---
+++ src/Practice/CozyApiPlayer.Core/ConsoleOutput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyApiPlayer.Core
+{
+    class ConsoleOutput : IResultOutput
+    {
+        public void onResult(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ src/Practice/CozyApiPlayer.Core/Project.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyApiPlayer.Core
+{
+    class Project
+    {
+        public List<Package> Reqs = new List<Package>();
+    }
+}
