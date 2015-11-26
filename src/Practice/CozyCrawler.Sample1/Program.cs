5fe1d92ab6434b2fc0b95741d098c2948aaec2a7
kingwl <805037171@163.com>
2015-11-26 22:43:14 +0800

    添加一个AsyncRunning

--- src/Practice/CozyCrawler.Sample1/Program.cs
+++ src/Practice/CozyCrawler.Sample1/Program.cs
@@ -13,9 +13,9 @@ namespace CozyCrawler.Sample1
     {
         static void Main(string[] args)
         {
-            SingleThreadGenerateUrlRunner p1 = new SingleThreadGenerateUrlRunner();
-            BlockedUrl2UrlRunner p2 = new BlockedUrl2UrlRunner();
-            BlockedUrl2ResultRunner p3 = new BlockedUrl2ResultRunner();
+            IGenerateUrlRunner p1   = new SingleThreadGenerateUrlRunner();
+            IUrl2UrlRunner p2       = new BlockedUrl2UrlRunner();
+            IUrl2ResultRunner p3    = new AsyncUrl2ResultRunner();
 
             p1.From(new TestUrlGenerater());
             p1.To(p2);
