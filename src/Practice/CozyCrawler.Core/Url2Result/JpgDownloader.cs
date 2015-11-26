6a61b6be9c7c1a7afa1996f74d10a88eb5cbfbf0
zapline <278998871@qq.com>
2015-11-26 19:18:30 +0800

    * 新爬虫轻松实现爬12306验证码

---
+++ src/Practice/CozyCrawler.Core/Url2Result/JpgDownloader.cs
@@ -0,0 +1,74 @@
+﻿using CozyCrawler.Interface;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyCrawler.Core.Url2Result
+{
+    public class JpgDownloader : IUrl2Result
+    {
+        void DoGetImage(string url, string path)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+
+            req.ServicePoint.Expect100Continue = false;
+            req.Method = "GET";
+            req.KeepAlive = true;
+
+            req.ContentType = "image/jpeg";
+            HttpWebResponse rsp = (HttpWebResponse)req.GetResponse();
+
+            System.IO.Stream stream = null;
+
+            try
+            {
+                // 以字符流的方式读取HTTP响应
+                stream = rsp.GetResponseStream();
+                Image.FromStream(stream).Save(path);
+            }
+            finally
+            {
+                // 释放资源
+                if (stream != null) stream.Close();
+                if (rsp != null) rsp.Close();
+            }
+        }
+
+        public static string GetMd5Hash(String input)
+        {
+            if (input == null)
+            {
+                return null;
+            }
+            MD5 md5Hash = MD5.Create();
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        string path_;
+        public void SetSavePath(string path)
+        {
+            path_ = path;
+        }
+
+        public void OnNewUrl(string url)
+        {
+            var md5 = GetMd5Hash(url);
+            if (md5 != null)
+            {
+                string tempPath = path_ + md5 + ".jpg";
+                DoGetImage(url, tempPath);
+            }
+        }
+    }
+}
