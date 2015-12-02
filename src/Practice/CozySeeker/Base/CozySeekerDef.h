44d3b55bde99e3520cf2c0e7b906d4b9437361b8
kingwl <805037171@163.com>
2015-12-02 23:54:35 +0800

    删除无用的导出定义

--- src/Practice/CozySeeker/Base/CozySeekerDef.h
+++ src/Practice/CozySeeker/Base/CozySeekerDef.h
@@ -1,12 +1,6 @@
 #ifndef __COZY_SEEKER_DEF__
 #define __COZY_SEEKER_DEF__
 
-#ifndef COZY_EXPORT
-#define COZY_API _declspec(dllimport)
-#else
-#define COZY_API _declspec(dllexport)
-#endif
-
 #define NS_BEGIN namespace Cozy {
 
 #define NS_END }
