7bca86fbfdba2defea10af2d3770204e4df98b0e
Max <763305825@qq.com>
2015-12-03 11:18:24 +0800

    微信简单验证

--- src/Practice/CozyOfficialAccounts.Server/Program.cs
+++ src/Practice/CozyOfficialAccounts.Server/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Nancy.Hosting.Self;
 
 namespace CozyOfficialAccounts.Server {
 
     class Program {
+
         static void Main(string[] args) {
+
+            using (var host = new NancyHost(new Uri("http://localhost:1024"))) {
+                host.Start();
+                Console.ReadKey();
+            }
         }
     }
 }
