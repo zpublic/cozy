475b4517226261f6dc1320ae910e6654980ac9cf
zapline <278998871@qq.com>
2015-12-03 18:21:15 +0800

    + CozyOfficialAccounts.Service

---
+++ src/Practice/CozyOfficialAccounts.Service/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CozyOfficialAccounts.Service.App_Start
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Weixin", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/Practice/CozyOfficialAccounts.Service/MessageHandles/CustomMessageHandle/CustomMessageHandle.cs
@@ -0,0 +1,23 @@
+﻿using Senparc.Weixin.MP.Entities;
+using Senparc.Weixin.MP.Entities.Request;
+using Senparc.Weixin.MP.MessageHandlers;
+using System.IO;
+
+namespace CozyOfficialAccounts.Service.MessageHandles.CustomMessageHandle
+{
+    public class CustomMessageHandler : MessageHandler<CustomMessageContext>
+    {
+        public CustomMessageHandler(Stream inputStream, PostModel postModel)
+            : base(inputStream, postModel)
+        {
+
+        }
+
+        public override IResponseMessageBase DefaultResponseMessage(IRequestMessageBase requestMessage)
+        {
+            var responseMessage = base.CreateResponseMessage<ResponseMessageText>();
+            responseMessage.Content = "这条消息来自DefaultResponseMessage。";
+            return responseMessage;
+        }
+    }
+}
