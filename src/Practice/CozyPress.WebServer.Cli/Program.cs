8f37378b535e2fd29d875302e16f1260977aa7de
zapline <278998871@qq.com>
2015-12-14 17:19:30 +0800

    * 修改CozyPress的host方式

--- src/Practice/CozyPress.WebServer.Cli/Program.cs
+++ src/Practice/CozyPress.WebServer.Cli/Program.cs
@@ -1,9 +1,7 @@
 ﻿using CozyPress.WebServer.Core;
+using Nancy;
+using Nancy.Hosting.Self;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CozyPress.WebServer.Cli
 {
@@ -11,8 +9,15 @@ namespace CozyPress.WebServer.Cli
     {
         static void Main(string[] args)
         {
-            CozyPressServer server = new CozyPressServer();
-            server.Run("http://localhost:12306");
+            CozyPressServer.Init();
+
+            HostConfiguration hostConfigs = new HostConfiguration()
+            {
+                UrlReservations = new UrlReservations() { CreateAutomatically = true },
+            };
+            var host = new NancyHost(new Uri("http://localhost:12306"), new DefaultNancyBootstrapper(), hostConfigs);
+            host.Start();
+
             Console.ReadKey();
         }
     }
--- src/Practice/CozyPress.WebServer.Core/CozyPressServer.cs
+++ src/Practice/CozyPress.WebServer.Core/CozyPressServer.cs
@@ -1,10 +1,4 @@
 ﻿using Nancy;
-using Nancy.Hosting.Self;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CozyPress.WebServer.Core
 {
@@ -21,16 +15,9 @@ namespace CozyPress.WebServer.Core
 
     public class CozyPressServer
     {
-        public void Run(string uri)
+        public static void Init()
         {
             CozyPressHolder.Instance.WhosYourDaddy();
-
-            HostConfiguration hostConfigs = new HostConfiguration()
-            {
-                UrlReservations = new UrlReservations() { CreateAutomatically = true },
-            };
-            var host = new NancyHost(new Uri(uri), new DefaultNancyBootstrapper(), hostConfigs);
-            host.Start();
         }
     }
 }
