c76ff0c6e1832c101884ed00b76afd95598eef41
zapline <278998871@qq.com>
2015-12-28 11:16:48 +0800

    * 开坑 CozyRemote

---
+++ src/Practice/CozyRemote.Puppet/Module/SystemApi/Misc.cs
@@ -0,0 +1,27 @@
+﻿using Nancy;
+using System;
+using System.Diagnostics;
+
+namespace CozyRemote.Puppet.Module.SystemApi
+{
+    public class Misc : NancyModule
+    {
+        public Misc() : base("/system/misc")
+        {
+            Get["/run/{exe}"] = x =>
+            {
+                string fileName = (string)x.exe;
+                var p = Process.Start(fileName);
+                if (p == null)
+                {
+                    return "failed";
+                }
+                else
+                {
+                    p.Close();
+                    return "success";
+                }
+            };
+        }
+    }
+}
