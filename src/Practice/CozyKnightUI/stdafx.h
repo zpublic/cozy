7ca50b68af5f2484f96504bdd74b57edc1a87c6c
kingwl <805037171@163.com>
2015-12-29 20:00:11 +0800

    [*]改为vs2005的工程

--- src/Practice/CozyKnightUI/stdafx.h
+++ src/Practice/CozyKnightUI/stdafx.h
@@ -5,17 +5,36 @@
 
 #pragma once
 
-#include "targetver.h"
+// 如果必须将位于下面指定平台之前的平台作为目标，请修改下列定义。
+// 有关不同平台对应值的最新信息，请参考 MSDN。
+#define WINVER			0x0500		// 将此值更改为相应的值，以适用于 Windows 的其他版本。
+#define _WIN32_WINNT	0x0501	// 将此值更改为相应的值，以适用于 Windows 的其他版本。
+#define _RICHEDIT_VER	0x0200
+#define _WIN32_IE		0x0600	// 将此值更改为相应的值，以适用于 IE 的其他版本。
 
-#define WIN32_LEAN_AND_MEAN             // 从 Windows 头中排除极少使用的资料
-// Windows 头文件: 
-#include <windows.h>
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NON_CONFORMING_SWPRINTFS
 
-// C 运行时头文件
-#include <stdlib.h>
-#include <malloc.h>
-#include <memory.h>
-#include <tchar.h>
+#define _WTL_NO_CSTRING
+#define _WTL_NO_WTYPES
+
+#include <atlbase.h>
+#include <atlstr.h>
+#include <atltypes.h>
+#include <atlapp.h>
+
+extern CAppModule _Module;
+#include<atlwin.h>
+#include <atlcrack.h>
+#include <atlsplit.h>
+#include <atlframe.h>
+#include <atlgdi.h>
+#include <atlctrls.h>
+#include <atlctrlx.h>
+#include <atlmisc.h>
+#include <tinyxml/tinyxml.h>
+#include "BkWin/CBkDialogViewImplEx.h"
+#include "bkres/bkres.h"
 
 
-// TODO:  在此处引用程序需要的其他头文件
+// TODO: 在此处引用程序需要的其他头文件
