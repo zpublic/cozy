4d155ea3ba5cc1a735e6ae037e4a03ef1db80205
kingwl <805037171@163.com>
2016-01-04 22:21:07 +0800

    [*]暂停一下进度

--- src/Practice/CozyKnightUI/CozyKnightSelectTargetDlg.cpp
+++ src/Practice/CozyKnightUI/CozyKnightSelectTargetDlg.cpp
@@ -1,4 +1,5 @@
 #include "CozyKnightSelectTargetDlg.h"
+#include "tlhelp32.h" 
 
 CozyKnightSelectTargetDlg::CozyKnightSelectTargetDlg(HANDLE hTarget)
     :CBkDialogViewImplEx<CozyKnightSelectTargetDlg>(IDR_SELECT_TARGET), m_hTarget(hTarget)
@@ -16,10 +17,28 @@ void CozyKnightSelectTargetDlg::OnBtnClose()
 
 void CozyKnightSelectTargetDlg::OnOk()
 {
-    AppendListItem(1001, "<text pos=\"0,0,-0,-0\">123</text>");
+   
 }
 
 void CozyKnightSelectTargetDlg::OnCalcle()
 {
     OnBtnClose();
+}
+
+void CozyKnightSelectTargetDlg::RefreshProcess()
+{
+    PROCESSENTRY32 pe32;
+    pe32.dwSize = sizeof(pe32);
+
+    HANDLE hProcessSnap = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if(hProcessSnap == INVALID_HANDLE_VALUE)
+    {
+        bool bMore = ::Process32First(hProcessSnap, &pe32);
+    }
+     AppendListItem(1001, "<text pos=\"0,0,-0,-0\">123</text>");
+}
+
+void CozyKnightSelectTargetDlg::OnInitDialog()
+{
+    RefreshProcess();
 }
