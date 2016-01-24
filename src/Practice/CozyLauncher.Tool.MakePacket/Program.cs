0d377f72b2136f5cf1b06ca9c880b91d71bc1cd1
zapline <278998871@qq.com>
2016-01-24 13:49:09 +0800

    + add CozyLauncher.Tool.MakePacket

---
+++ src/Practice/CozyLauncher.Tool.MakePacket/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyLauncher.Tool.MakePacket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filelist = new List<string> {
+                "CozyLauncher.exe",
+                "CozyLauncher.Core.dll",
+                "CozyLauncher.Infrastructure.dll",
+                "CozyLauncher.PluginBase.dll",
+
+                "NHotkey.dll",
+                "NHotkey.Wpf.dll",
+                "System.Windows.Interactivity.dll",
+
+                "CozyLauncher.Plugin.Core.dll",
+
+                "CozyLauncher.Plugin.Program.dll",
+                "CozyLauncher.Plugin.Dirctory.dll",
+                "CozyLauncher.Plugin.ManualRun.dll",
+                "CozyLauncher.Plugin.WebSearch.dll",
+
+                "CozyLauncher.Plugin.MouseClick.dll",
+            };
+
+            Directory.CreateDirectory("./cozy_launcher");
+            foreach (var f in filelist)
+            {
+                File.Copy(f, "./cozy_launcher/" + f);
+            }
+        }
+    }
+}
