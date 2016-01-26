4b21e3244658f09ec2b0dddb3cc78f65422552c7
kingwl <805037171@163.com>
2016-01-26 21:32:17 +0800

    [*]补充Update

--- src/Practice/CozyLauncher.Core/Update/UpdateMgr.cs
+++ src/Practice/CozyLauncher.Core/Update/UpdateMgr.cs
@@ -17,7 +17,8 @@ namespace CozyLauncher.Core.Update
     */
     public class UpdateMgr
     {
-        const string RemoteFileList = "http://www.laorouji.com/cozylauncher/a.list";
+        const string RemoteFileList = @"http://127.0.0.1:8000/hehe.json";
+        const string RemoteFilePath = @"http://127.0.0.1:8000/";
 
         List<FileVersionInfo> local_;
         List<FileVersionInfo> remote_;
@@ -26,17 +27,33 @@ namespace CozyLauncher.Core.Update
         {
             remote_ = GetRemoteFileVersionInfo();
             local_  = GetLocalFileVersionInfo();
+            var res = GetUpdateInfo(local_, remote_);
 
-            var fileDict = local_.ToDictionary(x => x.Name);
-            foreach(var file in remote_)
+            return res.Count > 0;
+        }
+
+        public static List<FileVersionInfo> GetUpdateInfo(IEnumerable<FileVersionInfo> local, IEnumerable<FileVersionInfo> remote)
+        {
+            var result      = new List<FileVersionInfo>();
+            var fileDict    = local.ToDictionary(x => x.Name);
+            foreach(var file in remote)
             {
-                if(!fileDict.ContainsKey(file.Name) || fileDict[file.Name].Md5 != file.Md5)
+                if (!fileDict.ContainsKey(file.Name) || fileDict[file.Name].Md5 != file.Md5)
                 {
-                    return true;
+                    result.Add(file);
                 }
             }
+            return result;
+        }
 
-            return false;
+        public string GetDownloadUrl(string filename)
+        {
+            return RemoteFilePath + filename;
+        }
+
+        public List<FileVersionInfo> GetUpdateResult()
+        {
+            return GetUpdateInfo(local_, remote_);
         }
 
         public List<FileVersionInfo> GetFileVersionInfo(bool bLocal = false)
---
+++ src/Practice/CozyLauncher.Tool.Update/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using CozyLauncher.Core.Update;
+using System.Windows.Input;
+using CozyLauncher.Tool.Update.Commands;
+using CozyLauncher.Infrastructure.Http;
+using System.IO;
+
+namespace CozyLauncher.Tool.Update.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        private int _UpdateCount;
+        public int UpdateCount
+        {
+            get
+            { return _UpdateCount; }
+            set
+            {
+                Set(ref _UpdateCount, value);
+            }
+        }
+
+        private int _UpdateNow;
+        public int UpdateNow
+        {
+            get
+            { return _UpdateNow; }
+            set
+            {
+                Set(ref _UpdateNow, value);
+            }
+        }
+
+        public ObservableCollection<FileVersionInfo> FileInfoList { get; set; }
+            = new ObservableCollection<FileVersionInfo>();
+
+        private UpdateMgr UpdateManager { get; set; } = new UpdateMgr();
+
+        private bool IsCancle { get; set; }
+
+        private ICommand _OkCommand;
+        public ICommand OkCommand
+        {
+            get
+            {
+                return _OkCommand = _OkCommand ?? new DelegateCommand(x =>
+                {
+                    Task.Run( () => 
+                    {
+                        if (UpdateManager.CheckUpdate())
+                        {
+                            // TODO Close CozyLauncher
+
+                            var res = UpdateManager.GetUpdateResult();
+
+                            UpdateCount = res.Count;
+                            UpdateNow = 0;
+                            IsCancle = false;
+
+                            var fn = Path.Combine("./", "backup/");
+                            if (!Directory.Exists(fn))
+                            {
+                                Directory.CreateDirectory(fn);
+                            }
+
+                            foreach (var file in res)
+                            {
+                                if (IsCancle)
+                                {
+                                    return;
+                                }
+
+                                System.Threading.Thread.Sleep(300);
+                                HttpDownload.HttpDownloadFile(UpdateManager.GetDownloadUrl(file.Name), fn + file.Name + ".cozy_update");
+                                UpdateNow++;
+                            }
+                        }
+                    });
+                });
+            }
+        }
+
+        private ICommand _CancleCommand;
+        public ICommand CancleCommand
+        {
+            get
+            {
+                return _CancleCommand = _CancleCommand ?? new DelegateCommand(x => 
+                {
+                    IsCancle = true;
+                });
+            }
+        }
+    }
+}
