09c038fd85c9c782f392b8111e71432e83ef9f82
zapline <278998871@qq.com>
2016-02-15 11:13:14 +0800

    + add CozyLauncher.Plugin.Qrcode

---
+++ src/Practice/CozyLauncher.Plugin.Qrcode/Main.cs
@@ -0,0 +1,49 @@
+﻿using CozyLauncher.PluginBase;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CozyLauncher.Plugin.Qrcode
+{
+    public class Main : IPlugin
+    {
+        private PluginInitContext context_;
+
+        public PluginInfo Init(PluginInitContext context)
+        {
+            context_ = context;
+            var info = new PluginInfo();
+            info.Keyword = "qr";
+            return info;
+        }
+
+        public List<Result> Query(Query query)
+        {
+            if (query.RawQuery.StartsWith("qr ") || query.RawQuery.StartsWith("qrcode "))
+            {
+                var rl = new List<Result>();
+                var r = new Result();
+                if (query.RawQuery.StartsWith("qr "))
+                {
+                    r.Title = query.RawQuery.Substring(3);
+                }
+                else
+                {
+                    r.Title = query.RawQuery.Substring(7);
+                }
+                r.SubTitle = "Show me the Qrcode";
+                r.IcoPath = "app";
+                r.Score = 80;
+                r.Action = e =>
+                {
+                    context_.Api.HideAndClear();
+                    QrcodeForm window = new QrcodeForm(r.Title);
+                    window.Show();
+                    return true;
+                };
+                rl.Add(r);
+                return rl;
+            }
+            return null;
+        }
+    }
+}
