9e66849493928aa18e507ef0c45d74cac81df765
Max <763305825@qq.com>
2016-02-16 17:13:37 +0800

    增加Nuint单元测试
    新增几个插件简单的单元测试

---
+++ src/Practice/CozyLauncher.Test.Plugin/FnlTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using CozyLauncher.PluginBase;
+
+namespace CozyLauncher.Plugin.Fnl.Tests {
+
+    [TestFixture()]
+    public class FnlTests {
+
+        [Test()]
+        public void FnlQueryTest() {
+            var main = new Main();
+            var result = main.Query(new Query { RawQuery = "fnl" });
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ src/Practice/CozyLauncher.Test.Plugin/IpTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using CozyLauncher.PluginBase;
+
+namespace CozyLauncher.Plugin.Ip.Tests {
+
+    [TestFixture()]
+    public class IpTests {
+
+        [Test()]
+        public void IpQueryTest() {
+            var main = new Main();
+            var result = main.Query(new Query { RawQuery = "ip" });
+            Assert.IsNotNull(result);
+        }
+    }
+}
