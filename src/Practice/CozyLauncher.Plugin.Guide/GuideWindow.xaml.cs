6f9612d0b2b91baf40ac93fd07d0fc6091ead428
kingwl <805037171@163.com>
2016-02-19 23:03:09 +0800

    [+]添加向导页

--- src/Practice/CozyLauncher.Plugin.Guide/GuideWindow.xaml.cs
+++ src/Practice/CozyLauncher.Plugin.Guide/GuideWindow.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System.Windows;
+using MahApps.Metro.Controls;
 
 namespace CozyLauncher.Plugin.Guide
 {
     /// <summary>
     /// GuideWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class GuideWindow : Window
+    public partial class GuideWindow : MetroWindow
     {
         public GuideWindow()
         {
