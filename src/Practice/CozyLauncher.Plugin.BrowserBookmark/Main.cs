0deae526e6a88aa08d2ff356797a9b58f4fe617e
zapline <278998871@qq.com>
2016-03-04 17:08:09 +0800

    + 计划 CozyLauncher.Plugin.BrowserBookmark

---
+++ src/Practice/CozyLauncher.Plugin.BrowserBookmark/Main.cs
@@ -0,0 +1,30 @@
+﻿using CozyLauncher.PluginBase;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace CozyLauncher.Plugin.BrowserBookmark
+{
+    public class Main : BasePlugin
+    {
+        private PluginInitContext context_;
+
+        public override PluginInfo Init(PluginInitContext context)
+        {
+            context_ = context;
+            var info = new PluginInfo();
+            info.Keyword = "b";
+            return info;
+        }
+
+        public override List<Result> Query(Query query)
+        {
+            if (query.RawQuery.StartsWith("b "))
+            {
+                var rl = new List<Result>();
+                return rl;
+            }
+            return null;
+        }
+    }
+}
