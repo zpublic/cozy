27cd93d29b295d1b05d9f847a8e8c075ac971c4c
zapline <278998871@qq.com>
2016-03-16 14:49:30 +0800

    + 完成CozyLauncher.Plugin.ManualRun的可配置

---
+++ src/Practice/CozyLauncher.Plugin.ManualRun/ActionData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace CozyLauncher.Plugin.ManualRun
+{
+    public class ActionData
+    {
+        public Dictionary<string, ActionOpenDirctory>   actionOpenDirctory;
+        public Dictionary<string, ActionOpenExe>        actionOpenExe;
+    }
+}
