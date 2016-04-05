e9df25c2b18a80d7a0a264291559e76a92485454
zapline <278998871@qq.com>
2016-04-05 12:49:09 +0800

    + CozyThunder接口走一波

---
+++ src/Practice/CozyThunder.Core/ICozyThunder.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+#include "ICozyThunderTask.h"
+
+class ICozyThunder
+{
+    virtual bool Initialize()                                       = 0;
+    virtual void UnInitialize()                                     = 0;
+
+    virtual ICozyThunderTask CreateTask(const wchar_t* sCfgPath)    = 0;
+    virtual ICozyThunderTask LoadTask(const wchar_t* sCfgPath)      = 0;
+    virtual bool SaveTask(ICozyThunderTask* pTask)                  = 0;
+
+    // 释放task
+    virtual bool ReleaseTask(ICozyThunderTask* pTask)               = 0;
+    // 清理task相关的缓存文件、配置文件，释放task，但不会清除下载完成的文件
+    virtual bool ClearTask(ICozyThunderTask* pTask)                 = 0;
+};
