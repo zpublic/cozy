7a43dbf1e6d0622e05ea4958a9a176d43bb6cefc
zapline <278998871@qq.com>
2016-04-07 19:18:23 +0800

    * 内网3M秒传测试通过

--- src/Practice/CozyThunder.Protocol/FileBlock/FileBlockDataPacket.cs
+++ src/Practice/CozyThunder.Protocol/FileBlock/FileBlockDataPacket.cs
@@ -3,8 +3,8 @@
     public class FileBlockDataPacket : Packet
     {
         internal static readonly int PacketId = 10002;
-        private const int PacketLength = 8 + 512;
-        public byte[] data512B;
+        private const int PacketLength = 8 + 3072;
+        public byte[] data3k;
 
         public FileBlockDataPacket()
         {
@@ -12,7 +12,7 @@
 
         public FileBlockDataPacket(byte[] data)
         {
-            data512B = data;
+            data3k = data;
         }
 
         public override void Encode(byte[] buffer, int offset)
@@ -20,12 +20,12 @@
             int written = offset;
             written += Write(buffer, written, PacketLength);
             written += Write(buffer, written, PacketId);
-            written += Write(buffer, written, data512B);
+            written += Write(buffer, written, data3k);
         }
 
         public override void Decode(byte[] buffer, int offset, int length)
         {
-            this.data512B = ReadBytes(buffer, offset + 8, length - 8);
+            this.data3k = ReadBytes(buffer, offset + 8, length - 8);
         }
 
         public override int ByteLength
