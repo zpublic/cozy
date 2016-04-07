1656b40c7b8a6784b081e793591140216a737ea9
zapline <278998871@qq.com>
2016-04-07 15:04:05 +0800

    * IPAddress.Any

--- src/Practice/CozyThunder.Botnet.Tester/Program.cs
+++ src/Practice/CozyThunder.Botnet.Tester/Program.cs
@@ -51,8 +51,8 @@ namespace CozyThunder.Botnet.Tester
             if (r == "master")
             {
                 MasterPeer slave = new MasterPeer();
-                slave.Start(IPAddress.Loopback, 48360, new MasterPeerListener());
-                var peer = new Peer() { EndPoint = new IPEndPoint(IPAddress.Loopback, 48361) };
+                slave.Start(IPAddress.Any, 48360, new MasterPeerListener());
+                var peer = new Peer() { EndPoint = new IPEndPoint(IPAddress.Parse("10.20.208.27"), 48361) };
                 slave.Connect(peer);
                 Console.ReadKey();
                 slave.Send(peer, "kingwl");
@@ -62,10 +62,10 @@ namespace CozyThunder.Botnet.Tester
             else if (r == "master2")
             {
                 MasterPeer slave = new MasterPeer();
-                slave.Start(IPAddress.Loopback, 48360, new MasterPeerListener());
-                var peer1 = new Peer() { EndPoint = new IPEndPoint(IPAddress.Loopback, 48361) };
-                var peer2 = new Peer() { EndPoint = new IPEndPoint(IPAddress.Loopback, 48362) };
-                var peer3 = new Peer() { EndPoint = new IPEndPoint(IPAddress.Loopback, 48363) };
+                slave.Start(IPAddress.Any, 48360, new MasterPeerListener());
+                var peer1 = new Peer() { EndPoint = new IPEndPoint(IPAddress.Parse("10.20.208.27"), 48361) };
+                var peer2 = new Peer() { EndPoint = new IPEndPoint(IPAddress.Parse("10.20.208.27"), 48362) };
+                var peer3 = new Peer() { EndPoint = new IPEndPoint(IPAddress.Parse("10.20.208.27"), 48363) };
                 slave.Connect(peer1);
                 slave.Connect(peer2);
                 slave.Connect(peer3);
@@ -79,7 +79,7 @@ namespace CozyThunder.Botnet.Tester
             else if (r == "slave")
             {
                 SlavePeer slave = new SlavePeer();
-                slave.Start(IPAddress.Loopback, 48361, new SlavePeerListener());
+                slave.Start(IPAddress.Any, 48361, new SlavePeerListener());
                 Console.ReadKey();
                 slave.Send("hehe");
                 Console.ReadKey();
@@ -88,9 +88,9 @@ namespace CozyThunder.Botnet.Tester
             else if (r == "slave2")
             {
                 SlavePeer slave = new SlavePeer();
-                slave.Start(IPAddress.Loopback, 48362, new SlavePeerListener());
+                slave.Start(IPAddress.Any, 48362, new SlavePeerListener());
                 SlavePeer slave2 = new SlavePeer();
-                slave2.Start(IPAddress.Loopback, 48363, new SlavePeerListener());
+                slave2.Start(IPAddress.Any, 48363, new SlavePeerListener());
                 Console.ReadKey();
                 slave.Stop();
                 slave2.Stop();
