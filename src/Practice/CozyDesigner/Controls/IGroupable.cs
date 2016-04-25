479ec0b7d5cf8e4718e13924f10fcb381e07a380
zapline <278998871@qq.com>
2016-04-25 21:45:23 +0800

    * CozyDesigner.DesignerCanvas

---
+++ src/Practice/CozyDesigner/Controls/IGroupable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CozyDesigner.Controls
+{
+    public interface IGroupable
+    {
+        Guid ID { get; }
+        Guid ParentID { get; set; }
+        bool IsGroup { get; set; }
+    }
+}
