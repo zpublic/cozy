5dbc14197abbda3d4346dffcbee91abe2737dadd
kingwl <805037171@163.com>
2016-04-26 23:43:30 +0800

    [+]添加peise.net的爬虫

---
+++ src/Practice/CozyCrawler.Peise/Palette.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozyCrawler.Peise
+{
+    public class Palette
+    {
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public List<string> RGB { get; set; } = new List<string>();
+    }
+}
---
+++ src/Practice/CozyCrawler.Peise/PeiseResult.cs
@@ -0,0 +1,38 @@
+﻿using CozyCrawler.Base;
+using CozyCrawler.Interface;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CozyCrawler.Peise
+{
+    class PeiseResult : IUrl2Result
+    {
+        private List<Palette> Data { get; set; }
+
+        public PeiseResult(List<Palette> data)
+        {
+            Data = data;
+        }
+
+        public void OnNewUrl(string url)
+        {
+            var obj = JsonConvert.DeserializeObject<Palette>(url);
+            Data.Add(obj);
+
+            lock (this)
+            {
+                Console.WriteLine(obj.Name);
+                foreach(var rgb in obj.RGB)
+                {
+                    Console.WriteLine(rgb);
+                }
+                Console.WriteLine("------------------------");
+            }
+        }
+    }
+}
