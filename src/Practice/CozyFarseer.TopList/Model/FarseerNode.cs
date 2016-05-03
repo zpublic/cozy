8cd8861fe2c62f3b7a282c21518106a2b234a5ee
zapline <278998871@qq.com>
2016-05-03 10:23:23 +0800

    + 开坑CozyFarseer.TopList

---
+++ src/Practice/CozyFarseer.TopList/Model/FarseerNode.cs
@@ -0,0 +1,22 @@
+﻿namespace CozyFarseer.TopList.Model
+{
+    public class FarseerNode
+    {
+        public int pid { get; set; }
+        public int uid { get; set; }
+        public string nick { get; set; }
+        public double accuracy { get; set; }
+        public string avatar { get; set; }
+        public int tag { get; set; }
+        public string content { get; set; }
+        public int time { get; set; }
+        public int likes { get; set; }
+        public string stock { get; set; }
+        public string sname { get; set; }
+        public int expires { get; set; }
+        public int countdown { get; set; }
+        public int ttype { get; set; }
+        public double tvalue { get; set; }
+        public int tstatus { get; set; }
+    }
+}
