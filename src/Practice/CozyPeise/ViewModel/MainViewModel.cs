259f1a41b6fd66ef11ba4f1aa1e2ab806d6b105f
zapline <278998871@qq.com>
2016-05-05 21:36:47 +0800

    * CozyPeise初步完工

--- src/Practice/CozyPeise/ViewModel/MainViewModel.cs
+++ src/Practice/CozyPeise/ViewModel/MainViewModel.cs
@@ -1,6 +1,9 @@
 using CozyPeise.Models;
 using GalaSoft.MvvmLight;
+using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 
 namespace CozyPeise.ViewModel
 {
@@ -8,24 +11,61 @@ namespace CozyPeise.ViewModel
     {
         public MainViewModel()
         {
-            var palette = new Palette()
+            if (File.Exists("Palette.dat"))
             {
-                Name = "hehe",
-                Url = "xxx",
-                RGB = new List<string>()
+                using (var fs = new FileStream("Palette.dat", FileMode.Open, FileAccess.Read))
                 {
-                    "#5FD9CD",
-                    "#EAF786",
-                    "#FFB5A1",
-                    "#B8FFB8",
-                    "#B8F4FF",
+                    using (var reader = new StreamReader(fs))
+                    {
+                        var res = JsonConvert.DeserializeObject<List<Palette>>(reader.ReadToEnd());
+                        _palettes = new ObservableCollection<PeiseListItemViewModel>();
+                        foreach (var i in res)
+                            _palettes.Add(new PeiseListItemViewModel(i));
+                    }
                 }
-            };
-            PeiseDisplayX = new PeiseDisplayViewModel(palette);
+            }
+            if (_palettes.Count > 0)
+            {
+                _curpeiselistitem = _palettes[0];
+                PeiseDisplayViewModel vm = new PeiseDisplayViewModel(_curpeiselistitem.Palette);
+                _curpeisedisplay = vm;
+            }
         }
 
         public string Title { get; } = "ÅäÉ«";
 
-        public PeiseDisplayViewModel PeiseDisplayX { get; }
+        ObservableCollection<PeiseListItemViewModel> _palettes;
+        public ObservableCollection<PeiseListItemViewModel> Palettes
+        {
+            get
+            {
+                return _palettes;
+            }
+            set
+            {
+                Set("Palettes", ref value);
+            }
+        }
+
+        PeiseListItemViewModel _curpeiselistitem;
+        public PeiseListItemViewModel CurPeiseListItem
+        {
+            get { return _curpeiselistitem; }
+            set
+            {
+                PeiseDisplayViewModel vm = new PeiseDisplayViewModel(value.Palette);
+                _curpeisedisplay = vm;
+                Set("CurPeiseDisplay", ref vm);
+            }
+        }
+
+        PeiseDisplayViewModel _curpeisedisplay;
+        public PeiseDisplayViewModel CurPeiseDisplay
+        {
+            get
+            {
+                return _curpeisedisplay;
+            }
+        }
     }
 }
