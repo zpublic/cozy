dd52cdb4c2d6fdd346513749eb6326ff27588c93
Max <max@xyzmax.xyz>
2016-05-10 11:55:01 +0800

    添加 RssFormatter.cs

--- src/Practice/CozyRSS.Syndication/Parser/Rss20FeedFormatter.cs
+++ src/Practice/CozyRSS.Syndication/Parser/Rss20FeedFormatter.cs
@@ -1,27 +1,17 @@
 ﻿using CozyRSS.Syndication.Model;
 using System.Xml;
 
-namespace CozyRSS.Syndication.Parser
-{
-    public class Rss20FeedFormatter
-    {
-        public static SyndicationFeed Parse(XmlNode node)
-        {
-            SyndicationFeed feed = new SyndicationFeed();
-            if (node?.Name == "channel")
-            {
-                feed.title = node.SelectSingleNode("title")?.InnerText;
-                feed.description = node.SelectSingleNode("description")?.InnerText;
-                feed.link = node.SelectSingleNode("link")?.InnerText;
-                feed.pubDate = node.SelectSingleNode("pubDate")?.InnerText;
-                feed.lastBuildDate = node.SelectSingleNode("lastBuildDate")?.InnerText;
-                feed.image = Rss20ImageFormatter.Parse(node.SelectSingleNode("image"));
-                foreach(XmlNode i in node.SelectNodes("item"))
-                {
-                    SyndicationItem item = Rss20ItemFormatter.Parse(i);
-                    if (item.IsValid())
-                        feed.items.Add(item);
-                }
+namespace CozyRSS.Syndication.Parser {
+
+    public class Rss20FeedFormatter {
+
+        public static SyndicationFeed Parse(XmlNode node) {
+
+            SyndicationFeed feed = RssFormatter.Parese<SyndicationFeed>(node);
+            foreach (XmlNode i in node.SelectNodes("item")) {
+                SyndicationItem item = RssFormatter.Parese<SyndicationItem>(i);
+                if (item.IsValid())
+                    feed.items.Add(item);
             }
             return feed;
         }
---
+++ src/Practice/CozyRSS.Syndication/Parser/RssFormatter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+
+namespace CozyRSS.Syndication.Parser {
+
+    public class RssFormatter {
+
+        public static TItem Parese<TItem>(XmlNode node) {
+            var item = Activator.CreateInstance<TItem>();
+            item.GetType().GetProperties()
+                .Where(x => !x.PropertyType.Name.StartsWith("ICollection"))
+                .ToList()
+                .ForEach(x => x.SetValue(item, node.SelectSingleNode(nameof(x.Name))?.InnerText));
+            return item;
+        }
+    }
+}
