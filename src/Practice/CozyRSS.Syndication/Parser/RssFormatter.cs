8029c10dc003fd5e121ced76e2d5c1346e809cab
Max <max@xyzmax.xyz>
2016-05-10 12:28:42 +0800

    RcozyRss.Syndication添加单元测试

--- src/Practice/CozyRSS.Syndication/Parser/RssFormatter.cs
+++ src/Practice/CozyRSS.Syndication/Parser/RssFormatter.cs
@@ -9,9 +9,9 @@ namespace CozyRSS.Syndication.Parser {
         public static TItem Parese<TItem>(XmlNode node) {
             var item = Activator.CreateInstance<TItem>();
             item.GetType().GetProperties()
-                .Where(x => !x.PropertyType.Name.StartsWith("ICollection"))
+                .Where(x => x.PropertyType.Name == "String")
                 .ToList()
-                .ForEach(x => x.SetValue(item, node.SelectSingleNode(nameof(x.Name))?.InnerText));
+                .ForEach(x => x.SetValue(item, node.SelectSingleNode(x.Name)?.InnerText));
             return item;
         }
     }
