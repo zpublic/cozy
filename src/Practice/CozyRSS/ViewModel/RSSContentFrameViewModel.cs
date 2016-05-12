dc6dceaacc09d2c1796e3d27f4018dcfbce55e26
zapline <278998871@qq.com>
2016-05-13 00:04:25 +0800

    * 写了一部分刷新读取feed

---
+++ src/Practice/CozyRSS/ViewModel/RSSContentFrameViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CozyRSS.Services;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+
+namespace CozyRSS.ViewModel
+{
+    public class RSSContentFrameViewModel : ViewModelBase
+    {
+        public RSSContentFrameViewModel()
+        {
+            UpdateContentCommand = new RelayCommand(() =>
+            {
+                // http://feed.cnblogs.com/blog/u/132703/rss
+                var feed = RssService.GetRssFeed("http://www.peise.net/rss.php?rssid=32");
+            });
+        }
+
+        public string Title { get; } = "老肉鸡的博客";
+
+        public RelayCommand UpdateContentCommand { get; }
+    }
+}
