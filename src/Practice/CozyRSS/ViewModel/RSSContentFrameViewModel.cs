a0100ed14e12e80c6e4f0a208e5fd52a50ccc31f
zapline <278998871@qq.com>
2016-05-13 09:34:46 +0800

    * CozyRSS开发记录15-获取和显示RSS内容

--- src/Practice/CozyRSS/ViewModel/RSSContentFrameViewModel.cs
+++ src/Practice/CozyRSS/ViewModel/RSSContentFrameViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CozyRSS.Services;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
 
 namespace CozyRSS.ViewModel
 {
@@ -12,11 +13,38 @@ namespace CozyRSS.ViewModel
             {
                 // http://feed.cnblogs.com/blog/u/132703/rss
                 var feed = RssService.GetRssFeed("http://www.peise.net/rss.php?rssid=32");
+                if (feed.items.Count > 0)
+                {
+                    _RSSContentList_ListItems.Clear();
+                    foreach (var i in feed.items)
+                    {
+                        _RSSContentList_ListItems.Add(new RSSContentList_ListItemViewModel(i));
+                    }
+                    Title = feed.title;
+                }
             });
         }
 
-        public string Title { get; } = "老肉鸡的博客";
+        string _title;
+        public string Title {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                Set("Title", ref value);
+            }
+        }
 
         public RelayCommand UpdateContentCommand { get; }
+
+        ObservableCollection<RSSContentList_ListItemViewModel> _RSSContentList_ListItems
+            = new ObservableCollection<RSSContentList_ListItemViewModel>();
+        public ObservableCollection<RSSContentList_ListItemViewModel> RSSListFrame_ListItems
+        {
+            get { return _RSSContentList_ListItems; }
+        }
     }
 }
---
+++ src/Practice/CozyRSS/ViewModel/RSSContentList_ListItemViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CozyRSS.Syndication.Model;
+using GalaSoft.MvvmLight;
+
+namespace CozyRSS.ViewModel
+{
+    public class RSSContentList_ListItemViewModel : ViewModelBase
+    {
+        SyndicationItem _item;
+        public RSSContentList_ListItemViewModel(SyndicationItem i)
+        {
+            _item = i;
+        }
+
+        public string Title { get { return _item?.title; } }
+        public string Time { get { return _item?.pubDate; } }
+        public string Icon { get; } = "StarOutline";
+    }
+}
