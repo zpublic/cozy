fd9ca70390d803ea975da43c520523aeb4f1e36d
kingwl <805037171@163.com>
2016-05-13 10:03:13 +0800

    [*]替换为JsonIgnore

--- src/Practice/CozyRSS.FeedManage/FeedCategory.cs
+++ src/Practice/CozyRSS.FeedManage/FeedCategory.cs
@@ -3,18 +3,15 @@ using System.Collections.Generic;
 
 namespace CozyRSS.FeedManage
 {
-    [JsonObject(MemberSerialization.OptIn)]
     public class FeedCategory
     {
-        [JsonProperty]
         public string name;
 
-        [JsonProperty]
         public List<FeedCategory> subCategories;
 
-        [JsonProperty]
         public List<FeedNode> subNodes;
 
+        [JsonIgnore]
         public FeedCategory parent;
     }
 }
