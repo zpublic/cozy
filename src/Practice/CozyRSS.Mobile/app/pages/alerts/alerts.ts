13b3bc30cd3514cbdf1ae063595f378154853834
zapline <278998871@qq.com>
2016-05-31 17:29:21 +0800

    * alerts完工

--- src/Practice/CozyRSS.Mobile/app/pages/alerts/alerts.ts
+++ src/Practice/CozyRSS.Mobile/app/pages/alerts/alerts.ts
@@ -1,17 +1,31 @@
 import {NavController, NavParams} from 'ionic-angular';
 import {Page} from 'ionic-angular';
 import {BasicAlertPage} from './basic';
+import {PromptAlertPage} from './prompt';
+import {ConfirmAlertPage} from './confirm';
+import {RadioAlertPage} from './radio';
+import {CheckboxAlertPage} from './checkbox';
 
 @Page({
   templateUrl: './build/pages/alerts/alerts.html'
 })
 export class AlertsPage {
-  items = [];
-
   constructor(public nav: NavController) {
   }
 
-  navBasicAlertPage(item) {
+  navBasicAlertPage() {
     this.nav.push(BasicAlertPage);
+  }
+  navPromptAlertPage() {
+    this.nav.push(PromptAlertPage);
+  }
+  navConfirmAlertPage() {
+    this.nav.push(ConfirmAlertPage);
+  }
+  navRadioAlertPage() {
+    this.nav.push(RadioAlertPage);
+  }
+  navCheckboxAlertPage() {
+    this.nav.push(CheckboxAlertPage);
   }
 }
