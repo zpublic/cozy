0de33dfd95a74a151442e090cb39aae978a7cc24
kingwl <805037171@163.com>
2016-06-01 14:47:16 +0800

    [+]searchbar

--- src/Practice/CozyRSS.Mobile/app/app.ts
+++ src/Practice/CozyRSS.Mobile/app/app.ts
@@ -14,13 +14,14 @@ import {LoadingPage} from './pages/loading/loading';
 import {ModalsPage} from './pages/modals/modals';
 import {RadiosPage} from './pages/radios/radios';
 import {ButtonsPage} from './pages/buttons/buttons';
+import {SearchBarsPage} from './pages/searchbars/searchbars'
 
 @App({
   template: '<ion-nav [root]="rootPage"></ion-nav>',
   config: {} // http://ionicframework.com/docs/v2/api/config/Config/
 })
 export class MyApp {
-  rootPage: any = ButtonsPage;
+  rootPage: any = SearchBarsPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
