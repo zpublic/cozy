d5cba91de1a7718da15a60c79caa9ea33427e5aa
zapline <278998871@qq.com>
2016-06-02 16:13:26 +0800

    * 整理cozyrss

--- src/Practice/CozyRSS.Mobile/app/app.ts
+++ src/Practice/CozyRSS.Mobile/app/app.ts
@@ -1,38 +1,14 @@
 import {App, Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
-import {MainPage} from './pages/main/main';
-import {ActionSheetsPage} from './pages/action-sheets/action-sheets';
-import {NavigationPage} from './pages/navigation/navigation';
-import {IconsPage} from './pages/icons/icons';
-import {AlertsPage} from './pages/alerts/alerts';
-import {BadgesPage} from './pages/badges/badges';
-import {CheckboxesPage} from './pages/checkboxes/checkboxes';
-import {DatetimePage} from './pages/datetime/datetime';
-import {GesturesPage} from './pages/gestures/gestures';
-import {GridPage} from './pages/grid/grid';
-import {LoadingPage} from './pages/loading/loading';
-import {ModalsPage} from './pages/modals/modals';
-import {RadiosPage} from './pages/radios/radios';
-import {ButtonsPage} from './pages/buttons/buttons';
-import {SearchBarsPage} from './pages/searchbars/searchbars'
-import {SelectsPage} from './pages/selects/selects'
-import {ToastPage} from './pages/toast/toast';
-import {ToastNativePage} from './native-pages/toast/toast';
-import {SplashscreenService} from './native-pages/splashscreen/splashscreen';
-import {DisplayDataAj2Page} from './angular-pages/display-data/display-data';
-import {UserInputAj2Page} from './angular-pages/user-input/user-input';
-import {FormsAj2Page} from './angular-pages/forms/forms';
-import {InputsPage} from './pages/inputs/inputs'
+import {CozyRssMainPage} from './cozyrss/mainpage'
 @App({
   template: '<ion-nav [root]="rootPage"></ion-nav>',
   config: {} // http://ionicframework.com/docs/v2/api/config/Config/
 })
 export class MyApp {
-  rootPage: any = InputsPage;
+  rootPage: any = CozyRssMainPage;
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      SplashscreenService.show();
-      SplashscreenService.hide();
       StatusBar.styleDefault();
     });
   }
--- src/Practice/CozyRSS.Mobile/app/pages/main/main.ts
+++ src/Practice/CozyRSS.Mobile/app/pages/main/main.ts
@@ -1,5 +1,26 @@
 import {Page} from 'ionic-angular';
-
+import {ActionSheetsPage} from '../../pages/action-sheets/action-sheets';
+import {NavigationPage} from '../../pages/navigation/navigation';
+import {IconsPage} from '../../pages/icons/icons';
+import {AlertsPage} from '../../pages/alerts/alerts';
+import {BadgesPage} from '../../pages/badges/badges';
+import {CheckboxesPage} from '../../pages/checkboxes/checkboxes';
+import {DatetimePage} from '../../pages/datetime/datetime';
+import {GesturesPage} from '../../pages/gestures/gestures';
+import {GridPage} from '../../pages/grid/grid';
+import {LoadingPage} from '../../pages/loading/loading';
+import {ModalsPage} from '../../pages/modals/modals';
+import {RadiosPage} from '../../pages/radios/radios';
+import {ButtonsPage} from '../../pages/buttons/buttons';
+import {SearchBarsPage} from '../../pages/searchbars/searchbars'
+import {SelectsPage} from '../../pages/selects/selects'
+import {ToastPage} from '../../pages/toast/toast';
+import {ToastNativePage} from '../../native-pages/toast/toast';
+import {SplashscreenService} from '../../native-pages/splashscreen/splashscreen';
+import {DisplayDataAj2Page} from '../../angular-pages/display-data/display-data';
+import {UserInputAj2Page} from '../../angular-pages/user-input/user-input';
+import {FormsAj2Page} from '../../angular-pages/forms/forms';
+import {InputsPage} from '../../pages/inputs/inputs'
 
 @Page({
   templateUrl: 'build/pages/main/main.html'
