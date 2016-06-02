8c0c7e6e76ea2f30d674383a2ea2a8e95c337dea
zapline <278998871@qq.com>
2016-06-02 17:05:52 +0800

    * 精简app.ts代码

--- src/Practice/CozyRSS.Mobile/app/app.ts
+++ src/Practice/CozyRSS.Mobile/app/app.ts
@@ -1,11 +1,6 @@
 import {App, Platform} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
 import {MainPage} from './pages/main/main';
-import {ToastNativePage} from './native-pages/toast/toast';
-import {SplashscreenService} from './native-pages/splashscreen/splashscreen';
-import {DisplayDataAj2Page} from './angular-pages/display-data/display-data';
-import {UserInputAj2Page} from './angular-pages/user-input/user-input';
-import {FormsAj2Page} from './angular-pages/forms/forms';
 import {CozyRssMainPage} from './cozyrss/mainpage'
 @App({
   template: '<ion-nav [root]="rootPage"></ion-nav>',
