955537e5fb8f191c3c6da2894903e073c4b112e5
kingwl <805037171@163.com>
2016-06-06 00:56:52 +0800

    [+] add setting

--- src/Practice/CozyRSS.Mobile/app/cozyrss/setting/setting.ts
+++ src/Practice/CozyRSS.Mobile/app/cozyrss/setting/setting.ts
@@ -1,10 +1,20 @@
-import {Page, Alert} from 'ionic-angular';
+import {Page, Alert, NavController} from 'ionic-angular';
 
 @Page({
   templateUrl: './build/cozyrss/setting/setting.html'
 })
 export class SettingPage {
-  onAboutClicked(){
-    
+  constructor(public nav: NavController) {
+
+  }
+
+  onAboutClicked() {
+    let alert = Alert.create({
+      title: '开发者:',
+      message: 'zapline、Kingwl、MaxTan',
+      buttons: ['好的']
+    });
+
+    this.nav.present(alert);
   }
 }
