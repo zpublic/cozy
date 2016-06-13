e13aefda789a36eed75446eeaf08ba5fe129a075
kingwl <805037171@163.com>
2016-06-13 22:53:06 +0800

    [+] add add favorite

--- src/Practice/CozyRSS.Mobile/app/cozyrss/rssdetail/rssdetail.ts
+++ src/Practice/CozyRSS.Mobile/app/cozyrss/rssdetail/rssdetail.ts
@@ -1,5 +1,6 @@
-import {Page, NavParams} from 'ionic-angular';
+import {Page, NavParams, NavController, Toast} from 'ionic-angular';
 import {RSSContent} from '../model';
+import {ModelService} from '../services/model.service';
 
 @Page({
   templateUrl: './build/cozyrss/rssdetail/rssdetail.html'
@@ -7,7 +8,36 @@ import {RSSContent} from '../model';
 export class RssDetailPage {
   content: RSSContent;
 
-  constructor(public params: NavParams) {
+  constructor(private params: NavParams, private models: ModelService, private nav: NavController) {
     this.content = params.data.item;
+  }
+
+  addFavorite() {
+    let _self = this;
+    this.models.mapFavorite(function (favorite: RSSContent[]) {
+      if (favorite.find(function (x) {
+        return x.url == _self.content.url;
+      }) == undefined) {
+        favorite.push(_self.content);
+
+        let toast = Toast.create({
+          message: 'add favorite success',
+          duration: 1000,
+        });
+
+        _self.nav.present(toast);
+      } else {
+        let toast = Toast.create({
+          message: 'favorite is existing',
+          duration: 1000,
+        });
+
+        _self.nav.present(toast);
+      }
+    })
+  }
+
+  copyToClipBoard() {
+
   }
 }
