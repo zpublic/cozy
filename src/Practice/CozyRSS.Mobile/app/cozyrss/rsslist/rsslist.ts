9cc1d3af1ae30ca9c5d7499a162946ee6350d3d0
kingwl <805037171@163.com>
2016-06-23 23:58:58 +0800

    [+] add refresh

--- src/Practice/CozyRSS.Mobile/app/cozyrss/rsslist/rsslist.ts
+++ src/Practice/CozyRSS.Mobile/app/cozyrss/rsslist/rsslist.ts
@@ -1,8 +1,10 @@
-import {Page, NavController} from 'ionic-angular';
+import {Page, NavController, Loading} from 'ionic-angular';
 import {RssSourcePage} from '../rsssource/rsssource';
 import {FavoritePage} from '../favorite/favorite';
-import {RSSSource} from '../model';
+import {RSSSource, RSSContent, FeedItem} from '../model';
 import {ModelService} from '../services/model.service';
+import {FeedService} from '../services/feed.service';
+import 'rxjs';
 
 @Page({
   templateUrl: './build/cozyrss/rsslist/rsslist.html',
@@ -10,8 +12,31 @@ import {ModelService} from '../services/model.service';
 export class RssListPage {
   rssList: RSSSource[];
 
-  constructor(private nav: NavController, private models: ModelService) {
+  constructor(private nav: NavController, private models: ModelService, private feeds: FeedService) {
     this.rssList = this.models.getSources();
+  }
+
+  private _updateSource(source: RSSSource, items: FeedItem[]) {
+    let count = 0;
+    let contents = items.map(function (item): RSSContent {
+      if (source.contents.find(function (content) {
+        return content.url == item.guid;
+      }) == undefined) {
+        count++;
+      }
+      return {
+        title: item.title,
+        url: item.guid,
+        time: item.pubDate,
+        author: item.author,
+        content: item.content,
+      };
+    });
+
+    source.news = count;
+    source.contents = contents;
+
+    return this.models.saveSources();
   }
 
   itemSelected(item) {
@@ -20,5 +45,32 @@ export class RssListPage {
 
   onFavorite() {
     this.nav.push(FavoritePage);
+  }
+
+  onRefresh() {
+    let loading = Loading.create({
+      content: 'refresh rss source',
+      dismissOnPageChange: true,
+    });
+
+    this.nav.present(loading);
+
+    let _self = this;
+    this.rssList.filter(function (x) {
+      return x.enable;
+    }).forEach(function (x) {
+
+      let source = x;
+      _self.feeds.readFeed(source.url)
+        .then(function (data) {
+          return _self._updateSource(x, data.items);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+        .then(function (x) {
+          loading.dismiss();
+        });
+    })
   }
 }
