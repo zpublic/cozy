e17c57fba0b0748e12cae145426bbcd0d2187366
zapline <278998871@qq.com>
2016-09-06 16:22:17 +0800

    * 用Ｃ＃版结巴分词来提取小说关键字

---
+++ src/Practice/CozyJieba.Play/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+open System.IO
+open System.Text
+open JiebaNet.Analyser
+open JiebaNet.Segmenter
+open Newtonsoft.Json
+
+[<EntryPoint>]
+let main argv =
+    let Obj2JsonIndented obj = JsonConvert.SerializeObject(obj, Formatting.Indented)
+
+    let fileName = @"C:\jiebanet\config\xiaoshuo1.txt"
+    let text = File.ReadAllText(fileName)
+    let tfidf = new TfidfExtractor();
+
+    tfidf.ExtractTags(text, 30, Constants.NounAndVerbPos)
+    |> Seq.cast<string>
+    |> Seq.toList
+    |> Obj2JsonIndented
+    |> (fun x -> File.WriteAllText(@"c:\jiebanet\config\xiaoshuo1_.txt", x))
+    0
