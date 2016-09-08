bcadee28d60669a7367a4cfe0543b78d1f55bfd1
zapline <278998871@qq.com>
2016-09-08 18:41:07 +0800

    + CozyIris开坑 图片处理

---
+++ src/Practice/CozyIris.Demo/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ImageProcessor;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace CozyIris.Demo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            byte[] photoBytes = File.ReadAllBytes(@"C:\Users\Administrator\Pictures\wwl.jpg");
+            BitmapImage bim = new BitmapImage();
+            bim.BeginInit();
+            bim.StreamSource = new MemoryStream(photoBytes);
+            bim.EndInit();
+            image.Source = bim;
+        }
+    }
+}
