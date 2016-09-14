b5e7fe0ff7ada5589df341cdfa7ee3d06a3bf71d
zapline <278998871@qq.com>
2016-09-14 15:21:04 +0800

    * 图片处理之ImageProcessor

---
+++ src/Practice/CozyIris.FDemo/Program.fs
@@ -0,0 +1,28 @@
+﻿open ImageProcessor
+open ImageProcessor.Processors
+open ImageProcessor.Imaging.Filters.Photo
+open System.Drawing
+
+[<EntryPoint>]
+let main argv = 
+    let image = @"d:\data\lulu.jpg"
+    let image2 = @"d:\data\lulu2.jpg"
+    let image3 = @"d:\data\lulu3.jpg"
+    let image4 = @"d:\data\lulu4.jpg"
+    let fac = new ImageFactory()
+
+    fac.Load(image)
+       .Resize(new Size(100, 100))
+       .Save(image2)
+       .Dispose()
+
+    fac.Load(image).Filter(MatrixFilters.BlackWhite).Save(image3).Dispose()
+
+    fac
+    |> fun x -> x.Load(image)
+    |> fun x -> x.Filter(MatrixFilters.Lomograph)
+    |> fun x -> x.Save(image4)
+    |> ignore
+
+    printfn "finish"
+    0
