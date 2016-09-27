e9a0d02f2f5caab52f36c4e4ae258df6ac396c67
zapline <278998871@qq.com>
2016-09-27 16:09:20 +0800

    + FlourishingTree生成

--- src/Practice/CozyArce.FractalTree/MainWindow.xaml.cs
+++ src/Practice/CozyArce.FractalTree/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using CozyArce.Tree.Generator.Sample;
 using CozyArce.Tree.Render.Sample;
 using CozyArce.Tree.Generator.SensitivePlant;
 using System.Windows;
+using CozyArce.Tree.Generator.FlourishingTree;
 
 namespace CozyArce.FractalTree
 {
@@ -27,6 +28,14 @@ namespace CozyArce.FractalTree
         private void button1_Click(object sender, RoutedEventArgs e)
         {
             g = new SensitivePlantGenerator();
+            r = new LowRender(xCanvas);
+            xCanvas.Children.Clear();
+            r.Draw(g.Generate());
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            g = new FlourishingTreeGenerator();
             r = new LowRender(xCanvas);
             xCanvas.Children.Clear();
             r.Draw(g.Generate());
