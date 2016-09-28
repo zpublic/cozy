76188992a26ebb9faffa5374fbc9994cbae2b5a0
zapline <278998871@qq.com>
2016-09-28 10:11:05 +0800

    + CozyArce.Uwp.DrawingBoard 用dx来画树，速度惊人

---
+++ src/Practice/CozyArce.Uwp.DrawingBoard/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
+using System.Numerics;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace CozyArce.Uwp.DrawingBoard
+{
+    public sealed partial class MainPage : Page
+    {
+        Random rnd = new Random();
+        private Vector2 RndPosition()
+        {
+            double x = rnd.NextDouble() * 500f;
+            double y = rnd.NextDouble() * 500f;
+            return new Vector2((float)x, (float)y);
+        }
+
+        private float RndRadius()
+        {
+            return (float)rnd.NextDouble() * 150f;
+        }
+
+        private byte RndByte()
+        {
+            return (byte)rnd.Next(256);
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        void canvas_Draw(
+            CanvasControl sender,
+            CanvasDrawEventArgs args)
+        {
+            CanvasCommandList cl = new CanvasCommandList(sender);
+            using (CanvasDrawingSession clds = cl.CreateDrawingSession())
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    clds.DrawText("Hello, World!", RndPosition(), Color.FromArgb(255, RndByte(), RndByte(), RndByte()));
+                    clds.DrawCircle(RndPosition(), RndRadius(), Color.FromArgb(255, RndByte(), RndByte(), RndByte()));
+                    clds.DrawLine(RndPosition(), RndPosition(), Color.FromArgb(255, RndByte(), RndByte(), RndByte()));
+                }
+            }
+            GaussianBlurEffect blur = new GaussianBlurEffect();
+            blur.Source = cl;
+            blur.BlurAmount = 1.0f;
+            args.DrawingSession.DrawImage(blur);
+        }
+
+        void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            canvas.RemoveFromVisualTree();
+            canvas = null;
+        }
+    }
+}
