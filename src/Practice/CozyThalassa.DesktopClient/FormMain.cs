4753d355dbcdf0990b816d0774b764c8d8383069
zapline <278998871@qq.com>
2016-10-12 17:45:32 +0800

    + 开坑CozyThalassa - 脑图形式的文档管理

---
+++ src/Practice/CozyThalassa.DesktopClient/FormMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace CozyThalassa.DesktopClient
+{
+    public partial class FormMain : Form
+    {
+        public ChromiumWebBrowser chromeBrowser;
+        public void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+            var page = string.Format(@"{0}\KityMinder.html", Application.StartupPath);
+            chromeBrowser = new ChromiumWebBrowser(page);
+            this.Controls.Add(chromeBrowser);
+            chromeBrowser.Dock = DockStyle.Fill;
+        }
+
+        public FormMain()
+        {
+            InitializeComponent();
+            InitializeChromium();
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+}
