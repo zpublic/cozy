49b8ff23b86c03ac3459ad2f77df13901a66020e
zapline <278998871@qq.com>
2016-11-01 09:18:16 +0800

    * CozyDump.InfoView做个简单界面，输出模块信息

--- src/Practice/CozyDump.InfoView/MainWindow.xaml.cs
+++ src/Practice/CozyDump.InfoView/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CozyDump.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,39 @@ using System.Windows.Shapes;
 
 namespace CozyDump.InfoView
 {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
     public partial class MainWindow : Window
     {
+        MiniDumpParser _dumpParser;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void buttonSelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.DefaultExt = ".dmp";
+            ofd.Filter = "dump file|*.dmp";
+            if (ofd.ShowDialog() == true)
+            {
+                textBoxDumpPath.Text = ofd.FileName;
+            }
+        }
+
+        private void buttonAnalyse_Click(object sender, RoutedEventArgs e)
+        {
+            _dumpParser?.Dispose();
+            _dumpParser = new MiniDumpParser();
+            _dumpParser.Parse(textBoxDumpPath.Text);
+            string output = "";
+            for (var i = 0; i < _dumpParser.ModuleNums; ++i)
+            {
+                var moduleName = _dumpParser.GetStringFromRva(_dumpParser.ModuleInfo(i).ModuleNameRva);
+                output += moduleName;
+                output += "\r\n";
+            }
+            textBlock.Text = output;
         }
     }
 }
